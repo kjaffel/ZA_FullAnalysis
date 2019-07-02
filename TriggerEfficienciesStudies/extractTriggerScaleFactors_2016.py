377f2a41708f1871a48edb1d722ca7d60b85ae10
kjaffel <khawla.jaffel@cern.ch>
2019-07-02 19:02:09 +0200

    script to extract the SFs

--- TriggerEfficienciesStudies/extractTriggerScaleFactors_2016.py
+++ TriggerEfficienciesStudies/extractTriggerScaleFactors_2016.py
@@ -1,5 +1,5 @@
 #! /bin/env python
-
+# files are found here : https://gitlab.cern.ch/cms-muonPOG/MuonReferenceEfficiencies?nav_source=navbar
 import math
 import sys
 import argparse
@@ -18,8 +18,6 @@ def extract_binning(data):
 
 def format_eta_bin(eta_bin):
     return 'abseta:[%.1f,%.1f]' % (eta_bin[0], eta_bin[1])
-def format_tag_bin(tag_bin):
-    return 'tag:[%.1f,%.1f]' % (tag_bin[0], tag_bin[1])
 
 def format_pt_bin(bin):
     return 'pt:[%.1f,%.1f]' % (bin[0], bin[1])
@@ -51,44 +49,13 @@ with open(args.file, 'r') as f:
     for wp, wp_data in d.items():
         print (wp, wp_data)
         print ('')
-        if 'histo_tag_nVertices_DATA'in wp_data.keys():
-            wp_data = wp_data['histo_tag_nVertices_DATA']
-            # Extract binning
-            tag_binning = extract_binning(wp_data)
-
-            json_content = {'dimension': 1, 'variables': ['tag'], 'binning': {'x': tag_binning}, 'data': [], 'error_type': 'absolute'}
-            json_content_data = json_content['data']
-            tag_data = { 'values': []}
-
-            for tag_bin_index in range(0, len(tag_binning) - 1):
-                tag_bin = format_tag_bin([tag_binning[tag_bin_index], tag_binning[tag_bin_index + 1]])
-                print (tag_bin)
-                if tag_bin not in wp_data:
-                    print('Error: eta bin not found in input file. This should not happen!')
-                    continue
-
-                content = wp_data[tag_bin]
-
-                value_data = { 'bin': [tag_binning[tag_bin_index], tag_binning[tag_bin_index + 1]], 'value': content['value'], 'error': content['error']}
-
-                tag_data['values'].append(value_data)
-                #print (value_data)
-            json_content_data.append(tag_data)
-            print (json_content_data)
-            filename = '%s_%s.json' % (wp, args.suffix)
-            with open(filename, 'w') as j:
-                import json
-                json.dump(json_content, j, indent=2)
-
-        #if not 'pt_abseta_ratio'in wp_data.keys(): //2016
-        #    continue
-        if not 'abseta_pt_DATA' in wp_data.keys():
+        if not 'pt_abseta_ratio' in wp_data.keys():
             continue
 
         print("Working on working point %r" % wp)
 
-        #wp_data = wp_data['abseta_pt_ratio'] //2016
-        wp_data = wp_data['abseta_pt_DATA'] 
+        wp_data = wp_data['abseta_pt_ratio']
+
         # Extract binning
         eta_binning = extract_binning(wp_data)
 
