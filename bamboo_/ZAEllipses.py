7679010ba87518a67d70abb3be2d2590d974bb40
kjaffel <khawla.jaffel@cern.ch>
2020-03-25 00:44:07 +0100

    boosted have been added and structure is modified

--- bamboo_/ZAEllipses.py
+++ bamboo_/ZAEllipses.py
@@ -1,60 +1,87 @@
 from bamboo.plots import Plot, SummedPlot 
 from bamboo.plots import EquidistantBinning as EqB
 from bamboo import treefunctions as op
+import utils
+from utils import safeget
 
-def MakeEllipsesPlots(self, TwoLeptonsTwoBjets, bestJetPairs, dilepton, channel, wp):
+def MakeEllipsesPLots(self, sel, bjets, lepton, wp, uname, suffix):
     plots = []
-    
-    for key in TwoLeptonsTwoBjets.keys():
-    
-        tagger = key.replace(wp, "")     
-
-        plots.append(Plot.make1D("jj_M_{0}_hZA_lljj_{1}_btag{2}_mll_and_met_cut".format(channel, tagger, wp),
-                    op.invariant_mass(bestJetPairs[tagger][0].p4, bestJetPairs[tagger][1].p4) , 
-                    TwoLeptonsTwoBjets.get(key), 
-                    EqB(100, 0., 1000.), 
-                    title="invariant mass of two b-tagged jets wrt {0} Discriminator".format(key), 
-                    xTitle= "mjj wrt {0} Discriminator [GeV]".format(key)))
+    binScaling=1
+    for key in sel.keys():
         
-        plots.append(Plot.make1D("lljj_M_{0}_hZA_lljj_{1}_btag{2}_mll_and_met_cut".format(channel, tagger, wp), 
-                    (dilepton[0].p4 +dilepton[1].p4+bestJetPairs[tagger][0].p4+bestJetPairs[tagger][1].p4).M(),
-                    TwoLeptonsTwoBjets.get(key), 
-                    EqB(100, 0., 1000.), 
-                    title="invariant mass of 2 leptons two b-tagged jets wrt {0} Discriminator".format(key), 
-                    xTitle="mlljj wrt {0} Discriminator [GeV]".format(key)))
+        tagger = key.replace(wp, "")
+        bjets_ = safeget(bjets, tagger, wp)
         
-        plots.append(Plot.make2D("Mjj_vs_Mlljj_{0}_hZA_lljj_{1}_btag{2}_mll_and_met_cut".format(channel, tagger, wp), 
-                    (op.invariant_mass(bestJetPairs[tagger][0].p4, bestJetPairs[tagger][1].p4),(
-                    dilepton[0].p4 +dilepton[1].p4+bestJetPairs[tagger][0].p4+bestJetPairs[tagger][1].p4).M()),
-                    TwoLeptonsTwoBjets.get(key), 
-                    (EqB(100, 0., 1000.), 
-                    EqB(100, 0., 1000.)), 
-                    title="mlljj vs mjj invariant mass wrt {0} Discriminator".format(key)))
-                
-        plots.append(Plot.make1D("ll_M_{0}_hZA_lljj_{1}_btag{2}_mll_and_met_cut".format(channel, tagger, wp), 
-                    op.invariant_mass(dilepton[0].p4, dilepton[1].p4), 
-                    TwoLeptonsTwoBjets.get(key), 
-                    EqB(100, 70., 110.), 
-                    title=" dilepton invariant mass wrt {0} Discriminator".format(key), 
-                    xTitle= "mll wrt {0} Discriminator [GeV]".format(key)))
+        plots.append(Plot.make1D("jj_M_{0}_{1}_hZA_lljj_{2}_mll_and_met_cut".format(suffix, uname, key),
+                    op.invariant_mass(bjets_[0].p4, bjets_[1].p4), 
+                    sel.get(key), 
+                    EqB(60 // binScaling, 0., 1000.), 
+                    title="invariant mass of two b-tagged jets wrt {0} Discriminator".format(suffix, key), 
+                    xTitle= "mjj {0} {1} [GeV]".format(suffix, key),
+                    plotopts=utils.getOpts(uname, **{"log-y": False})))
+        
+        plots.append(Plot.make1D("lljj_M_{0}_{1}_hZA_lljj_{2}_mll_and_met_cut".format(suffix, uname, key), 
+                    (lepton[0].p4 + lepton[1].p4 + bjets_[0].p4 + bjets_[1].p4).M(),
+                    sel.get(key), 
+                    EqB(60 // binScaling, 0., 1000.), 
+                    title="invariant mass of 2 leptons two b-tagged jets wrt {0} Discriminator".format(suffix, key), 
+                    xTitle="mlljj {0} {1} [GeV]".format(suffix, key),
+                    plotopts=utils.getOpts(uname, **{"log-y": False})))
+        
+        plots.append(Plot.make2D("Mjj_vs_Mlljj_{0}_{1}_hZA_lljj_{2}_mll_and_met_cut".format(suffix, uname, key), 
+                    (op.invariant_mass(bjets_[0].p4, bjets_[1].p4),(
+                    lepton[0].p4 + lepton[1].p4 + bjets_[0].p4 + bjets_[1].p4).M()),
+                    sel.get(key), 
+                    (EqB(60 // binScaling, 0., 1000.), EqB(60 // binScaling, 0., 1000.)), 
+                    title="mlljj vs mjj invariant mass {0} wrt {1} Discriminator".format(suffix, key),
+                    plotopts=utils.getOpts(uname, **{"log-y": False})))
+        
+        plots.append(Plot.make1D("ll_M_{0}_{1}_hZA_lljj_{2}_mll_and_met_cut".format(suffix, uname, key), 
+                    op.invariant_mass(lepton[0].p4, lepton[1].p4), 
+                    sel.get(key), 
+                    EqB(60 // binScaling, 70., 110.), 
+                    title=" dilepton invariant mass {0} wrt {1} Discriminator".format(suffix, key), 
+                    xTitle= "mll {0} {1} [GeV]".format(suffix, key),
+                    plotopts=utils.getOpts(uname)))
     return plots
 
-def MakeMETPlots(self, TwoLeptonsTwoBjets_NoMETCut, corrMET, MET, channel):
+def MakeMETPlots(self, sel, corrmet, met, uname, suffix):    
     plots = []
-    # Tagger here include the wp 
-    for tagger in TwoLeptonsTwoBjets_NoMETCut.keys():
+    binScaling=1
+    for key in sel.keys():
+        
+        plots.append(Plot.make1D("met_pt_{0}_{1}_hZA_lljj_{2}".format(suffix, uname, key), 
+                    met.pt, sel.get(key), 
+                    EqB(60 // binScaling, 0., 600.), title="MET p_{T} [GeV]",
+                    plotopts=utils.getOpts(uname, **{"log-y": False})))
+        plots.append(Plot.make1D("xycorrmet_pt_{0}_{1}_hZA_lljj_{2}".format(suffix, uname, key), 
+                    corrmet.pt, sel.get(key), 
+                    EqB(60 // binScaling, 0., 600.), title="corrMET p_{T} [GeV]",
+                    plotopts=utils.getOpts(uname, **{"log-y": False})))
+    return plots
 
-        plots.append(Plot.make1D("met_pt_{0}_hZA_lljj_btag_{1}".format(channel, tagger), 
-                    MET.pt, 
-                    TwoLeptonsTwoBjets_NoMETCut.get(tagger), 
-                    EqB(50, 0., 400.), 
-                    title=" Missing Transverse Energy", 
-                    xTitle= "MET p_{T} [GeV]"))
-                
-        plots.append(Plot.make1D("xycorrmet_pt_{0}_hZA_lljj_btag_{1}".format(channel, tagger), 
-                    corrMET.pt, 
-                    TwoLeptonsTwoBjets_NoMETCut.get(tagger), 
-                    EqB(50, 0., 400.), 
-                    title=" x-y correction on Missing Transverse Energy", 
-                    xTitle= "corrMET p_{T} [GeV]"))
+def MakeExtraMETPlots(self, sel, lepton, met, uname, suffix):
+    binScaling=1
+    plots = []
+    for key in sel.keys():
+
+        plots.append(Plot.make1D("{0}_{1}_{2}_MET_pt".format(uname, key, suffix), met.pt, sel.get(key),
+                        EqB(60 // binScaling, 0., 600.), title="MET p_{T} [GeV]",
+                        plotopts=utils.getOpts(uname, **{"log-y": False})))
+        plots.append(Plot.make1D("{0}_{1}_{2}_MET_phi".format(uname, key, suffix), met.phi, sel.get(key),
+                        EqB(60 // binScaling, -3.1416, 3.1416), title="MET #phi",
+                        plotopts=utils.getOpts(uname, **{"log-y": False})))
+        plots.append(Plot.make1D("{0}_{1}_{2}_MET_eta".format(uname, key, suffix), met.phi, sel.get(key),
+                        EqB(60 // binScaling, -2.4, 2.4), title="MET #eta",
+                        plotopts=utils.getOpts(uname, **{"log-y": False})))
+        
+        for i in range(2):
+            plots.append(Plot.make1D(f"{uname}_{key}_{suffix}_MET_lep{i+1}_deltaPhi".format(uname=uname, key=key, suffix=suffix),
+                            op.Phi_mpi_pi(lepton[i].phi - met.phi), sel.get(key), EqB(60 // binScaling, -3.1416, 3.1416),
+                            title="#Delta #phi (lepton, MET)", plotopts=utils.getOpts(uname, **{"log-y": False})))
+    
+            MT = op.sqrt( 2. * met.pt * lepton[i].p4.Pt() * (1. - op.cos(op.Phi_mpi_pi(met.phi - lepton[i].p4.Phi()))) )
+            plots.append(Plot.make1D(f"{uname}_{key}_{suffix}_MET_MT_lep{i+1}".format(uname=uname, key=key, suffix=suffix), MT, sel.get(key),
+                            EqB(60 // binScaling, 0., 600.), title="Lepton M_{T} [GeV]",
+                            plotopts=utils.getOpts(uname, **{"log-y": False})))
     return plots
