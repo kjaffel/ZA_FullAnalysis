dd2df761da0b51b9bbbe9bc1a85fab2b8246555a
kjaffel <khawla.jaffel@cern.ch>
2020-11-30 18:50:17 +0100

    update skimmer with bjet energy regression and the btagweight

--- bamboo_/skimmedtree_ZAtollbb.py
+++ bamboo_/skimmedtree_ZAtollbb.py
@@ -11,11 +11,16 @@ if zabPath not in sys.path:
     sys.path.append(zabPath)
 
 from ZAtollbb import NanoHtoZABase
+from b_regression import scalesfactorsLIB
 import utils
 from utils import safeget
 
 import logging
 logger = logging.getLogger("ZA SKIMMER")
+
+from bamboo.root import addIncludePath, loadHeader
+addIncludePath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "include"))
+loadHeader("BTagEffEvaluator.h")
 
 def value_for_channel(d, channel, return_dilepton):
     """Return a value in neseted dic `d` having a key == channel ."""
@@ -28,6 +33,8 @@ def value_for_channel(d, channel, return_dilepton):
             else:
                 return sel
 
+def bJetEnergyRegression(jets):
+    return op.map(jets, lambda j : op.construct("ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<float>>", (j.pt*j.bRegCorr, j.eta, j.phi, j.mass*j.bRegCorr)))
 
 class Skimedtree_NanoHtoZA(NanoHtoZABase, NanoAODSkimmerModule):
     def __init__(self, args):
@@ -48,9 +55,11 @@ class Skimedtree_NanoHtoZA(NanoHtoZABase, NanoAODSkimmerModule):
         parser.add_argument("-wp",  "--workingpoints", default=None, help="Setting Working point is mandatory")
     
     def defineSkimSelection(self, t, noSel, sample=None, sampleCfg=None):
+        from bamboo.root import addIncludePath, loadHeader
+        from bamboo.scalefactors import BtagSF
 
-        noSel, PUWeight, categories, isDY_reweight, WorkingPoints, deepBFlavScaleFactor, deepB_AK4ScaleFactor, AK4jets, AK8jets, bjets_resolved, bjets_boosted, CleanJets_fromPileup, electrons, muons, MET, corrMET, PuppiMET, elRecoSF_highpt, elRecoSF_lowpt = self.defineObjects(t, noSel, sample, sampleCfg)
-
+        noSel, PUWeight, categories, isDY_reweight, WorkingPoints, btagging, deepBFlavScaleFactor, deepB_AK4ScaleFactor, deepB_AK8ScaleFactor, AK4jets, AK8jets, fatjets_nosubjettinessCut, bjets_resolved, bjets_boosted, CleanJets_fromPileup, electrons, muons, MET, corrMET, PuppiMET, elRecoSF_highpt, elRecoSF_lowpt = self.defineObjects(t, noSel, sample, sampleCfg)
+        
         era = sampleCfg["era"]
         isMC = self.isMC(sample)
         
@@ -59,7 +68,7 @@ class Skimedtree_NanoHtoZA(NanoHtoZABase, NanoAODSkimmerModule):
             sys.exit(0)
 
         if self.SetRegion not in ["boosted", "resolved"]:
-            print ( '[Skimedtree_NanoHtoZA]: Region of studies should be : boosted or resolved ! ' )
+            print ('[Skimedtree_NanoHtoZA]: Region of studies should be : boosted or resolved ! ' )
             sys.exit(0)
         
 
@@ -88,13 +97,11 @@ class Skimedtree_NanoHtoZA(NanoHtoZABase, NanoAODSkimmerModule):
                 sys.exit(1)
         
         if self.SetRegion== "resolved":
-            jets= AK4jets
-            bjets= bjets_resolved
+            jets= bJetEnergyRegression (AK4jets)
             suffix = "AK4Jets"
         
         elif self.SetRegion=="boosted":
             jets= AK8jets
-            bjets= bjets_boosted
             suffix= "AK8Jets"
         
         else:
@@ -133,9 +140,11 @@ class Skimedtree_NanoHtoZA(NanoHtoZABase, NanoAODSkimmerModule):
             RawMET = t.MET if era != "2017" else t.METFixEE2017
             varsToKeep["RawMET_pt"]  = RawMET.pt
             varsToKeep["RawMET_phi"] = RawMET.phi
+            varsToKeep["RawMET_eta"] = RawMET.eta
                 # xy corr
             varsToKeep["CorrMET_pt"]  = corrMET.pt
-            varsToKeep["CorrMET_phi"] = corrMET.phi
+            varsToKeep["CorrMET_phi"] = CorrMET.phi
+            varsToKeep["CorrMET_eta"] = corrMET.eta
     
         ### Opposite sign leptons , Same Flavour  selection 
         dilepton = value_for_channel(categories, self.SetCat, True)
@@ -157,39 +166,114 @@ class Skimedtree_NanoHtoZA(NanoHtoZABase, NanoAODSkimmerModule):
                 "resolved": TwoLeptonsTwoJets_Resolved,
                 "boosted" : TwoLeptonsOneJet_Boosted
                 }
+        
+        pathtoRoOtmaps = { '2016': "/home/ucl/cp3/kjaffel/scratch/ZAFullAnalysis/2016Results/ver.20_10_08/results/summedProcessesForEffmaps/summedProcesses_2016_ratios.root",
+                            '2017': "/home/ucl/cp3/kjaffel/scratch/ZAFullAnalysis/2017Results/ver.20_10_06/fix_bug_cause_missing_histograms/results/summedProcessesForEffmaps/summedProcesses_2017_ratios.root",
+                            '2018': "/home/ucl/cp3/kjaffel/scratch/ZAFullAnalysis/2018Results/ver.20_10_06/fix_bug_causing_missingHistograms/results/summedProcessesForEffmaps/summedProcesses_2018_ratios.root"
+                        }
+        
+        cleaned_AK4JetsByDeepFlav = op.sort(AK4jets, lambda j: -j.btagDeepFlavB)
+        cleaned_AK4JetsByDeepB = op.sort(AK4jets, lambda j: -j.btagDeepB)
+        cleaned_AK8JetsByDeepB = op.sort(AK8jets, lambda j: -j.btagDeepB)
+
         for wp in WorkingPoints:
+            idx = ( 0 if wp=="L" else ( 1 if wp=="M" else 2))
+            if os.path.exists(pathtoRoOtmaps[era]):
+                bTagEff_deepcsvAk4 = op.define("BTagEffEvaluator", 'const auto <<name>> = BTagEffEvaluator("%s", "%s", "resolved", "deepcsv", {%s});'%(pathtoRoOtmaps[era], wp, btagging['DeepCSV'][era][idx]))
+                bTagEff_deepflavour = op.define("BTagEffEvaluator", 'const auto <<name>> = BTagEffEvaluator("%s", "%s", "resolved", "deepflavour", {%s});'%(pathtoRoOtmaps[era], wp, btagging['DeepFlavour'][era][idx]))
+                if 'T' not in wp:
+                    bTagEff_deepcsvAk8 = op.define("BTagEffEvaluator", 'const auto <<name>> = BTagEffEvaluator("%s", "%s", "boosted", "deepcsv", {%s});'%(pathtoRoOtmaps[era], wp, btagging['DeepCSV'][era][idx]))
+            else:
+                raise RuntimeError(f"{era} efficiencies maps not found !")
+                
+            csv_deepcsvAk4 = scalesfactorsLIB['DeepCSV']['Ak4'][era]
+            csv_deepcsvSubjets = scalesfactorsLIB['DeepCSV']['softdrop_subjets'][era]
+            csv_deepflavour = scalesfactorsLIB['DeepFlavour'][era]
+            OP= ("Loose" if wp=='L' else ("Medium" if wp=='M' else "Tight"))
+    
+            btagSF_deepcsv= BtagSF('deepcsv', csv_deepcsvAk4, wp=OP, sysType="central", otherSysTypes=["up", "down"],
+                                                    systName= f'mc_eff_deepcsv{wp}', measurementType={"B": "comb", "C": "comb", "UDSG": "incl"}, sel= noSel,
+                                                    uName=f'sf_eff_{self.SetCat}_On{sample}_deepcsv{wp}')
+            btagSF_deepflavour= BtagSF('deepflavour', csv_deepflavour, wp=OP, sysType="central", otherSysTypes=["up", "down"],
+                                                    systName= f'mc_eff_deepflavour{wp}', measurementType={"B": "comb", "C": "comb", "UDSG": "incl"}, sel= noSel,
+                                                    uName=f'sf_eff_{self.SetCat}_On{sample}_deepflavour{wp}')
+            if 'Tight' not in OP : 
+                btagSF_subjets= BtagSF('deepcsv', csv_deepcsvSubjets, wp=OP, sysType="central", otherSysTypes=["up", "down"],
+                                                    systName= f'mc_eff_subjets_deepcsv{wp}', measurementType={"B": "lt", "C": "lt", "UDSG": "incl"}, sel= noSel,
+                                                    uName=f'sf_eff_{self.SetCat}_On{sample}_subjets_deepcsv{wp}')
+    
+            deepcsv_bTagWeight = None
+            deepflavour_bTagWeight = None
+            deepcsvAk8_bTagWeight = None
+
+            # resolved 
             bJets_resolved_PassdeepflavourWP=safeget(bjets_resolved, "DeepFlavour", wp)
             bJets_resolved_PassdeepcsvWP=safeget(bjets_resolved, "DeepCSV", wp)
+            # boosted
             bJets_boosted_PassdeepcsvWP=safeget(bjets_boosted, "DeepCSV", wp)
+    
+            if isMC:
+                bTagSF_DeepCSVPerJet = op.map(cleaned_AK4JetsByDeepB, 
+                                                lambda j: bTagEff_deepcsvAk4.evaluate( j.hadronFlavour, j.btagDeepB, j.pt*j.bRegCorr, op.abs(j.eta), btagSF_deepcsv(j)))
+                bTagSF_DeepFlavourPerJet = op.map(cleaned_AK4JetsByDeepFlav, 
+                                                    lambda j: bTagEff_deepflavour.evaluate(j.hadronFlavour, j.btagDeepFlavB, j.pt*j.bRegCorr, op.abs(j.eta), btagSF_deepflavour(j)))
+                bTagSF_DeepCSVPerSubJet = op.map(cleaned_AK8JetsByDeepB, 
+                                                lambda j: op.product(
+                                                                bTagEff_deepcsvAk8.evaluate( 
+                                                                        op.static_cast("BTagEntry::JetFlavor", 
+                                                                                    op.multiSwitch((j.nBHadrons >0, op.c_int(5)), 
+                                                                                                    (j.nCHadrons >0, op.c_int(4)), 
+                                                                                                    op.c_int(0)) ), 
+                                                                            j.subJet1.btagDeepB, 
+                                                                            j.subJet1.pt, 
+                                                                            op.abs(j.subJet1.eta), 
+                                                                            btagSF_subjets(j)) , 
+                                                                bTagEff_deepcsvAk8.evaluate( 
+                                                                        op.static_cast("BTagEntry::JetFlavor",
+                                                                                    op.multiSwitch((j.nBHadrons >0, op.c_int(5)), 
+                                                                                        (j.nCHadrons >0, op.c_int(4)), 
+                                                                                        op.c_int(0)) ), 
+                                                                            j.subJet2.btagDeepB, 
+                                                                            j.subJet2.pt, 
+                                                                            op.abs(j.subJet2.eta), 
+                                                                            btagSF_subjets(j))  )
+                                                                )
+                deepcsv_bTagWeight = op.rng_product(bTagSF_DeepCSVPerJet)
+                deepflavour_bTagWeight = op.rng_product(bTagSF_DeepFlavourPerJet)
+                deepcsvAk8_bTagWeight = op.rng_product(bTagSF_DeepCSVPerSubJet)
+    
+            run2_bTagEventWeight_PerWP = { 'resolved': { 
+                                                'DeepCSV{0}'.format(wp): deepcsv_bTagWeight,
+                                                'DeepFlavour{0}'.format(wp): deepflavour_bTagWeight },
+                                            'boosted' :  {'DeepCSV{0}'.format(wp): deepcsvAk8_bTagWeight }
+                                    }
             
-            print ( bJets_resolved_PassdeepflavourWP, bjets_resolved, '**********************')
-            TwoLeptonsTwoBjets_NoMETCut_Res = {
-                "DeepFlavour{0}".format(wp) :  lljjSelections["resolved"].refine("TwoLeptonsTwoBjets_NoMETcut_DeepFlavour{0}_{1}_Resolved".format(wp, self.SetCat),
-                                                                        cut=[ op.rng_len(bJets_resolved_PassdeepflavourWP) > 1 ],
-                                                                        weight=([ deepBFlavScaleFactor(bJets_resolved_PassdeepflavourWP[0]), 
-                                                                                  deepBFlavScaleFactor(bJets_resolved_PassdeepflavourWP[1])
-                                                                                ] if isMC else None
-                                                                                )),
-
-                "DeepCSV{0}".format(wp)     :  lljjSelections["resolved"].refine("TwoLeptonsTwoBjets_NoMETcut_DeepCSV{0}_{1}_Resolved".format(wp, self.SetCat),
-                                                                        # here where the selection become exclusive 
-                                                                        cut=[ op.rng_len(bJets_resolved_PassdeepcsvWP) > 1, op.rng_len(bJets_boosted_PassdeepcsvWP) ==0],
-                                                                        weight=([ deepB_AK4ScaleFactor(bJets_resolved_PassdeepcsvWP[0]), 
-                                                                                  deepB_AK4ScaleFactor(bJets_resolved_PassdeepcsvWP[1])
-                                                                                ] if isMC else None
-                                                                                ))
-                                            }
-
-
-            TwoLeptonsOneBjets_NoMETCut_Boo = {
-                "DeepCSV{0}".format(wp)     :  lljjSelections["boosted"].refine("TwoLeptonsOneBjets_NoMETcut_DeepCSV{0}_{1}_Boosted".format(wp, self.SetCat),
-                                                                        cut=[ op.rng_len(bJets_boosted_PassdeepcsvWP) > 0 ])
-                                                }
-        llbbSelections_noMETCut = {
-                "resolved": TwoLeptonsTwoBjets_NoMETCut_Res,
-                "boosted" : TwoLeptonsOneBjets_NoMETCut_Boo
-                }
+            
+            TwoLeptonsTwoBjets_NoMETCut_bTagEventWeight_Res = {
+                "DeepFlavour{0}".format(wp) :  lljjSelections["resolved"].refine("TwoLeptonsTwoBjets_NoMETcut_bTagEventWeight_DeepFlavour{0}_{1}_Resolved".format(wp, self.SetCat),
+                                                                cut=[ op.rng_len(bJets_resolved_PassdeepflavourWP) > 1 ], 
+                                                                    weight=run2_bTagEventWeight_PerWP['resolved']["DeepFlavour{0}".format(wp)]),
+    
+                "DeepCSV{0}".format(wp)     :  lljjSelections["resolved"].refine("TwoLeptonsTwoBjets_NoMETcut_bTagEventWeight_DeepCSV{0}_{1}_Resolved".format(wp, self.SetCat),
+                                                                cut=[ op.rng_len(bJets_resolved_PassdeepcsvWP) > 1, op.rng_len(bJets_boosted_PassdeepcsvWP) ==0], 
+                                                                    weight=run2_bTagEventWeight_PerWP['resolved']["DeepCSV{0}".format(wp)])
+                                        }
+    
+            TwoLeptonsOneBjets_NoMETCut_bTagEventWeight_Boo = {
+                "DeepCSV{0}".format(wp)     :  lljjSelections["boosted"].refine("TwoLeptonsOneBjets_NoMETcut_bTagEventWeight_DeepCSV{0}_{1}_Boosted".format(wp, self.SetCat),
+                                                                    cut=[ op.rng_len(bJets_boosted_PassdeepcsvWP) > 0 ], 
+                                                                weight=run2_bTagEventWeight_PerWP['boosted']["DeepCSV{0}".format(wp)])
+                                        }
+                        
+            llbbSelections_NoMETCut_bTagEventWeight = { "resolved": TwoLeptonsTwoBjets_NoMETCut_bTagEventWeight_Res,
+                                                    "boosted" : TwoLeptonsOneBjets_NoMETCut_bTagEventWeight_Boo }
                 
+            llbbSelections_METCut_bTagEventWeight = { reg:
+                                                        { key: selbTag.refine(f"TwoLeptonsTwoBjets_METCut_bTagEventWeight_{key}_{self.SetCat}_{reg}", cut=[corrMET.pt <80. ])
+                                                        for key, selbTag in bTagEventWeight_selections.items() }
+                                                                for reg, bTagEventWeight_selections in llbbSelections_NoMETCut_bTagEventWeight.items()
+                                                                } 
+            
         # boosted or resolved 
         ### Two OS SF Leptons _ Two Jets  selection inclusive 
         if self.SetSel=="2Lep2Jets":
@@ -210,17 +294,24 @@ class Skimedtree_NanoHtoZA(NanoHtoZABase, NanoAODSkimmerModule):
             ### Two OS SF Leptons _ Two bJets  selection + no MET cut
         elif self.SetSel=="2Lep2bJets":
             key = self.SetTagger + self.SetWP
-            bJets = safeget(bjets, self.SetTagger, self.SetWP)
-            FinalSel = safeget( llbbSelections_noMETCut [self.SetRegion], key)
+            FinalSel = safeget( llbbSelections_METCut_bTagEventWeight [self.SetRegion], key)
             
             if self.SetRegion=="resolved":
-                llbb_M= (dilepton[0].p4 +dilepton[1].p4+bJets[0].p4+bJets[1].p4).M()
-                bb_M= op.invariant_mass(bJets[0].p4+bJets[1].p4)
+                
+                bjets = safeget(bjets_resolved, self.SetTagger, self.SetWP)
+                bJets= bJetEnergyRegression (bjets)
+                
+                llbb_M= (dilepton[0].p4 +dilepton[1].p4+bJets[0]+bJets[1]).M()
+                bb_M= op.invariant_mass(bJets[0]+bJets[1])
             
             elif self.SetRegion=="boosted":
-                llbb_M= (dilepton[0].p4 +dilepton[1].p4+bJets[0].p4).M()
-                bb_M= op.invariant_mass(bJets[0].p4)
 
+                bJets = safeget(bjets_boosted, self.SetTagger, self.SetWP)
+                llbb_M= (dilepton[0].p4 +dilepton[1].p4+bJets[0].p4).M()
+                bb_M= bJets[0].mass
+                bb_softDropM= bJets[0].msoftdrop
+    
+                varsToKeep["bb_softDropM"]= bb_softDropM
             
             varsToKeep["llbb_M"]= llbb_M
             varsToKeep["bb_M"]= bb_M
