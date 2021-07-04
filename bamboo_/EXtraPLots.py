ddd009cd817046a1cf0b2e74368b7e0d3da0c6dc
kjaffel <khawla.jaffel@cern.ch>
2021-07-04 13:15:09 +0200

    mv rhoPlots to EXtraPLots

--- bamboo_/EXtraPLots.py
+++ bamboo_/EXtraPLots.py
@@ -1,15 +1,16 @@
 import sys
 import os
-from bambooToOls import Plot
 from bamboo.plots import SummedPlot
 from bamboo.plots import EquidistantBinning as EqB
+from bamboo.root import gbl
 from bamboo import treefunctions as op
+from bambooToOls import Plot
 
 zabPath = os.path.dirname(__file__)
 if zabPath not in sys.path:
     sys.path.append(zabPath)
-import utils
 
+import utils
 class makeYieldPlots:
     def __init__(self):
         self.calls = 0
@@ -30,6 +31,26 @@ class makeYieldPlots:
         self.calls += 1
     def returnPlots(self):
         return self.plots
+
+
+def makerhoPlots(selections, bjets, leptons, ellipses, ellipse_params, suffix, cut, WP, uname):
+    plots = []
+
+    for key, sel in selections.items():
+        tagger = key.replace(WP, "")
+        bjets_ = bjets[key.replace(WP, "")][WP]
+        
+        bb_p4= ((bjets_[0].p4+bjets_[1].p4) if suffix=="resolved" else( bjets_[0].p4))
+        bb_M = bb_p4.M()
+        llbb_M = (leptons[0].p4 +leptons[1].p4+bb_p4).M()
+        for j, line in enumerate(ellipse_params, 0): 
+            MH = str(line[-1]).replace('.', 'p')
+            MA = str(line[-2]).replace('.', 'p')
+            plots.append(Plot.make1D(f"rho_steps_{suffix}_histo_{uname}_hZA_lljj_{tagger}_btag{WP}_{cut}_MH_{MH}_MA_{MA}", 
+                            ellipses.at(op.c_int(j)).radius(bb_M, llbb_M),
+                            sel, EqB(6, 0., 3.), title="rho ",
+                            plotopts=utils.getOpts(uname, **{"log-y": True})))
+    return plots
 
 def MakeTriggerDecisionPlots(catSel, channel):
     """
