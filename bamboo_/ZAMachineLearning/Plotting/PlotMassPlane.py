1906dd0870b9b050c87919593e192b81e9ac0900
kjaffel <khawla.jaffel@cern.ch>
2021-11-04 21:17:43 +0100

    update mass plane script

--- bamboo_/ZAMachineLearning/Plotting/PlotMassPlane.py
+++ bamboo_/ZAMachineLearning/Plotting/PlotMassPlane.py
@@ -2,11 +2,14 @@ import os
 import sys
 import argparse
 import copy
-import numpy as np
-from root_numpy import hist2array
+import json
 import ROOT
 
+import numpy as np
+from root_numpy import hist2array
 sys.path.append(os.path.abspath('..'))
+
+import Operations
 from talos import Restore
 from preprocessing import PreprocessLayer
 from tdrstyle import setTDRStyle
@@ -28,10 +31,8 @@ class MassPlane:
         self.plot_ZA    = plot_ZA
         self.profile    = profile
         self.graph_list = []
-
         # Produce grid #
         self.produce_grid()
-
 
     def produce_grid(self):
         self.X,self.Y = np.meshgrid(np.linspace(self.x_min,self.x_max,self.x_bins),np.linspace(self.y_min,self.y_max,self.y_bins))
@@ -43,17 +44,37 @@ class MassPlane:
         # X, Y are 2D arrays, x,y are vectors of points
 
     def load_model(self,path_model):
-        print(path_model)
-        self.model = Restore(path_model, custom_objects={'PreprocessLayer': PreprocessLayer}).model
+        self.model  = Restore(path_model, custom_objects={name:getattr(Operations,name) for name in dir(Operations) if name.startswith('op')}).model
+        #self.model = Restore(path_model, custom_objects={'PreprocessLayer': PreprocessLayer}).model
         self.model_name = os.path.basename(path_model).replace(".zip","")
-
+        print(f'Path to model : {path_model}, {self.model_name} will be used as name for plotting')
 
     def plotMassPoint(self,mH,mA):
         print ("Producing plot for MH = %.2f GeV, MA = %.2f"%(mH,mA))
         N = self.x.shape[0]
         params = np.c_[np.ones(N)*mA,np.ones(N)*mH]
-        inputs = np.c_[self.x,self.y,params]
-        output = self.model.predict(inputs)
+        #'l1_pdgId@op_pdgid', '$era@op_era', 'bb_M','llbb_M','bb_M_squared','llbb_M_squared', 'bb_M_x_llbb_M','$mA', '$mH'
+        pdgid = 11.
+        era   = 2016. 
+        bb_M_squared   = pow(self.x,2)                                                    
+        llbb_M_squared = pow(self.y,2)
+        bb_M_x_llbb_M  = self.x * self.y
+        pdgid = pdgid*np.ones(N)
+        era   = era*np.ones(N)
+        #print( pdgid, type(pdgid), pdgid.shape)
+        #print( era, type(era) , era.shape)
+        #print( self.x , type(self.x), self.x.shape)
+        #print( self.y , type(self.y), self.y.shape)
+        #print( bb_M_squared , type(bb_M_squared), bb_M_squared.shape)
+        #print( llbb_M_squared, type(llbb_M_squared), llbb_M_squared.shape)
+        #print( bb_M_x_llbb_M, type(self.x * self.y), bb_M_x_llbb_M.shape)
+
+        v = np.c_[pdgid, era]
+        variables = [bb_M_squared, llbb_M_squared, bb_M_x_llbb_M, self.x,self.y, mA, mH]
+        inputsLL  = np.c_[v, bb_M_squared, llbb_M_squared, bb_M_x_llbb_M, self.x,self.y,params]
+        #print( inputsLL )
+        inputs    = np.hsplit(inputsLL,inputsLL.shape[1])
+        output    = self.model.predict(inputs)
 
         g_DY = ROOT.TGraph2D(N)
         g_DY.SetNpx(500)
@@ -140,12 +161,14 @@ class MassPlane:
 
         return xproj, yproj
 
-
-    def plotOnCanvas(self):
+    def plotOnCanvas(self, path_model):
         setTDRStyle()
-        pdf_path = "MassPlane/"+self.model_name+".pdf"
+        path_out  = path_model.split('model')[0] + "plots/MassPlane/"
+        if not os.path.isdir(path_out):
+            os.makedirs(path_out)
+        pdf_path  = path_out + self.model_name + ".pdf"
         root_path = pdf_path.replace('.pdf','.root')
-        outFile = ROOT.TFile(root_path,"RECREATE")
+        outFile   = ROOT.TFile(root_path,"RECREATE")
         C = ROOT.TCanvas("C","C",800,600)
         #C.SetLogz()
         C.Print(pdf_path+"[")
@@ -171,12 +194,15 @@ class MassPlane:
         outFile.Close()
         print ("Root file saved as %s"%root_path)
 
-    def makePavement(self,contours):
+    def makePavement(self,contours, path_model):
+        path_out  = path_model.split('model')[0] + "plots/MassPlane/"
+        if not os.path.isdir(path_out):
+            os.makedirs(path_out)
         for contour in contours:
             print ("Producing pavement for cut %.2f"%contour)
-            pdf_path = "MassPlane/"+self.model_name+("_pave%0.2f"%contour).replace('.','p')+".pdf"
+            pdf_path  = path_out + self.model_name+("_pave%0.2f"%contour).replace('.','p')+".pdf"
             root_path = pdf_path.replace('.pdf','.root')
-            outFile = ROOT.TFile(root_path,"RECREATE")
+            outFile   = ROOT.TFile(root_path,"RECREATE")
             C = ROOT.TCanvas("C","C",800,600)
             opt = 'cont2'
             new_graph_list = [g.Clone() for g in self.graph_list]
@@ -196,15 +222,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Script to plot mass plane with Neural Net')
     parser.add_argument('--model', action='store', required=True, type=str, 
                    help='Path to model (in zip file)')
-    parser.add_argument('-mA', action='store', required=False, type=int, 
+    parser.add_argument('--mA', action='store', required=False, type=int, 
                    help='Mass of A for plot')
-    parser.add_argument('-mH', action='store', required=False, type=int, 
+    parser.add_argument('--mH', action='store', required=False, type=int, 
                    help='Mass of H for plot')
-    parser.add_argument('-DY', action='store_true', required=False, default=False,
+    parser.add_argument('--DY', action='store_true', required=False, default=True,
                    help='Wether to plot the DY output')
-    parser.add_argument('-TT', action='store_true', required=False, default=False,
+    parser.add_argument('--TT', action='store_true', required=False, default=True,
                    help='Wether to plot the TT output')
-    parser.add_argument('-ZA', action='store_true', required=False, default=False,
+    parser.add_argument('--ZA', action='store_true', required=False, default=True,
                    help='Wether to plot the ZA output')
     parser.add_argument('--profile', action='store_true', required=False, default=False,
                    help='Wether also plot the profiles in MH and MA')
@@ -220,238 +246,13 @@ if __name__ == "__main__":
     if args.mA and args.mH:
         inst.plotMassPoint(args.mH,args.mA)
     elif not args.gif:
-        masspoints = [(132.0, 30.0),
-                      (132.0, 37.34),
-                      (143.44, 30.0),
-                      (143.44, 37.34),
-                      (143.44, 46.48),
-                      (157.77, 30.0),
-                      (157.77, 37.34),
-                      (157.77, 46.48),
-                      (157.77, 57.85),
-                      (173.52, 30.0),
-                      (173.52, 37.34),
-                      (173.52, 46.48),
-                      (173.52, 57.85),
-                      (173.52, 72.01),
-                      (190.85, 30.0),
-                      (190.85, 37.34),
-                      (190.85, 46.48),
-                      (190.85, 57.85),
-                      (190.85, 71.28),
-                      (190.85, 86.78),
-                      (200.0, 50.0),
-                      (200.0, 100.0),
-                      (209.9, 30.0),
-                      (209.9, 37.34),
-                      (209.9, 46.48),
-                      (209.9, 57.71),
-                      (209.9, 71.15),
-                      (209.9, 86.79),
-                      (209.9, 104.53),
-                      (230.77, 30.0),
-                      (230.77, 37.1),
-                      (230.77, 45.88),
-                      (230.77, 56.73),
-                      (230.77, 69.78),
-                      (230.77, 85.09),
-                      (230.77, 102.72),
-                      (230.77, 123.89),
-                      (250.0, 50.0),
-                      (250.0, 100.0),
-                      (261.4, 37.1),
-                      (261.4, 45.88),
-                      (261.4, 56.73),
-                      (261.4, 69.66),
-                      (261.4, 85.1),
-                      (261.4, 102.99),
-                      (261.4, 124.53),
-                      (261.4, 150.5),
-                      (296.1, 30.0),
-                      (296.1, 36.79),
-                      (296.1, 45.12),
-                      (296.1, 55.33),
-                      (296.1, 67.65),
-                      (296.1, 82.4),
-                      (296.1, 99.9),
-                      (296.1, 120.82),
-                      (296.1, 145.93),
-                      (296.1, 176.02),
-                      (300.0, 50.0),
-                      (300.0, 100.0),
-                      (300.0, 200.0),
-                      (335.4, 30.0),
-                      (335.4, 36.79),
-                      (335.4, 45.12),
-                      (335.4, 55.33),
-                      (335.4, 67.54),
-                      (335.4, 82.14),
-                      (335.4, 99.61),
-                      (335.4, 120.39),
-                      (335.4, 145.06),
-                      (335.4, 174.55),
-                      (335.4, 209.73),
-                      (379.0, 30.0),
-                      (379.0, 36.63),
-                      (379.0, 44.72),
-                      (379.0, 54.59),
-                      (379.0, 66.57),
-                      (379.0, 80.99),
-                      (379.0, 98.26),
-                      (379.0, 118.81),
-                      (379.0, 143.08),
-                      (379.0, 171.71),
-                      (379.0, 205.76),
-                      (379.0, 246.3),
-                      (442.63, 30.0),
-                      (442.63, 36.64),
-                      (442.63, 44.76),
-                      (442.63, 54.67),
-                      (442.63, 66.49),
-                      (442.63, 80.03),
-                      (442.63, 95.27),
-                      (442.63, 113.53),
-                      (442.63, 135.44),
-                      (442.63, 161.81),
-                      (442.63, 193.26),
-                      (442.63, 230.49),
-                      (442.63, 274.57),
-                      (442.63, 327.94),
-                      (500.0, 50.0),
-                      (500.0, 100.0),
-                      (500.0, 200.0),
-                      (500.0, 300.0),
-                      (500.0, 400.0),
-                      (516.94, 30.0),
-                      (516.94, 36.47),
-                      (516.94, 44.34),
-                      (516.94, 53.9),
-                      (516.94, 65.52),
-                      (516.94, 78.52),
-                      (516.94, 93.12),
-                      (516.94, 109.3),
-                      (516.94, 128.58),
-                      (516.94, 151.69),
-                      (516.94, 179.35),
-                      (516.94, 212.14),
-                      (516.94, 250.63),
-                      (516.94, 296.65),
-                      (516.94, 352.61),
-                      (516.94, 423.96),
-                      (609.21, 30.0),
-                      (609.21, 34.86),
-                      (609.21, 40.51),
-                      (609.21, 47.08),
-                      (609.21, 54.71),
-                      (609.21, 63.58),
-                      (609.21, 85.86),
-                      (609.21, 99.78),
-                      (609.21, 116.29),
-                      (609.21, 135.66),
-                      (609.21, 158.41),
-                      (609.21, 185.18),
-                      (609.21, 216.52),
-                      (609.21, 253.68),
-                      (609.21, 298.01),
-                      (609.21, 351.22),
-                      (609.21, 417.76),
-                      (609.21, 505.93),
-                      (650.0, 50.0),
-                      (717.96, 30.0),
-                      (717.96, 34.86),
-                      (717.96, 40.51),
-                      (717.96, 47.08),
-                      (717.96, 54.71),
-                      (717.96, 63.58),
-                      (717.96, 73.89),
-                      (717.96, 85.86),
-                      (717.96, 99.78),
-                      (717.96, 116.19),
-                      (717.96, 157.56),
-                      (717.96, 183.48),
-                      (717.96, 213.73),
-                      (717.96, 249.34),
-                      (717.96, 291.34),
-                      (717.96, 341.02),
-                      (717.96, 400.03),
-                      (717.96, 475.8),
-                      (717.96, 577.65),
-                      (800.0, 50.0),
-                      (800.0, 100.0),
-                      (800.0, 200.0),
-                      (800.0, 400.0),
-                      (800.0, 700.0),
-                      (846.11, 30.0),
-                      (846.11, 34.93),
-                      (846.11, 40.68),
-                      (846.11, 47.37),
-                      (846.11, 55.16),
-                      (846.11, 64.24),
-                      (846.11, 74.8),
-                      (846.11, 87.1),
-                      (846.11, 101.43),
-                      (846.11, 118.11),
-                      (846.11, 137.54),
-                      (846.11, 160.17),
-                      (846.11, 186.51),
-                      (846.11, 217.19),
-                      (846.11, 252.91),
-                      (846.11, 294.51),
-                      (846.11, 345.53),
-                      (846.11, 405.4),
-                      (846.11, 475.64),
-                      (846.11, 558.06),
-                      (846.11, 654.75),
-                      (997.14, 30.0),
-                      (997.14, 34.93),
-                      (997.14, 40.68),
-                      (997.14, 47.37),
-                      (997.14, 55.16),
-                      (997.14, 64.24),
-                      (997.14, 74.8),
-                      (997.14, 87.1),
-                      (997.14, 101.43),
-                      (997.14, 118.11),
-                      (997.14, 137.54),
-                      (997.14, 160.17),
-                      (997.14, 186.51),
-                      (997.14, 217.19),
-                      (997.14, 254.82),
-                      (997.14, 298.97),
-                      (997.14, 350.77),
-                      (997.14, 411.54),
-                      (997.14, 482.85),
-                      (997.14, 566.51),
-                      (997.14, 664.66),
-                      (997.14, 779.83),
-                      (1000.0, 50.0),
-                      (1000.0, 200.0),
-                      (1000.0, 500.0)]
+        #with open(os.path.join('data','points_0.500000_0.500000.json')) as f:
+        #   d = json.load(f)
+        #   masspoints = [(mH, mA,) for mA, mH in d]
+        masspoints = [( 300, 50), ( 300, 100), ( 300, 200),]
         for masspoint in masspoints:
             inst.plotMassPoint(*masspoint)
-        #inst.plotMassPoint(200,50)
-        #inst.plotMassPoint(200,100)
-        #inst.plotMassPoint(250,50)
-        #inst.plotMassPoint(250,100)
-        #inst.plotMassPoint(300,50)
-        #inst.plotMassPoint(300,100)
-        #inst.plotMassPoint(300,200)
-        #inst.plotMassPoint(500,50)
-        #inst.plotMassPoint(500,100)
-        #inst.plotMassPoint(500,200)
-        #inst.plotMassPoint(500,300)
-        #inst.plotMassPoint(500,400)
-        #inst.plotMassPoint(650,50)
-        #inst.plotMassPoint(800,50)
-        #inst.plotMassPoint(800,100)
-        #inst.plotMassPoint(800,200)
-        #inst.plotMassPoint(800,400)
-        #inst.plotMassPoint(800,700)
-        #inst.plotMassPoint(1000,50)
-        #inst.plotMassPoint(1000,100)
-        #inst.plotMassPoint(1000,200)
-        #inst.plotMassPoint(1000,500)
-
-    inst.plotOnCanvas()
+    
+    inst.plotOnCanvas(args.model)
     if args.pavement is not None:
-        inst.makePavement(args.pavement)
+        inst.makePavement(args.pavement, args.model)
