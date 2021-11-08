803ea4bee024cdde8534d31645e9dd5a22a3de3e
kjaffel <khawla.jaffel@cern.ch>
2021-11-08 16:18:35 +0100

    onnx still show issues with LCG101 , tf version 2.5 and keras2onnx 1.9 ==> so I make this script work for both conversion keras --> tf or keras --> onnx  : juts add --job you need -- waiting until this issue to be fixed

--- bamboo_/ZAMachineLearning/ML-Tools/Keras2TensorFlow2Onnx.py
+++ bamboo_/ZAMachineLearning/ML-Tools/Keras2TensorFlow2Onnx.py
@@ -24,7 +24,7 @@ from preprocessing import PreprocessLayer
 print("TensorFlow version is "+tf.__version__)
 print("keras2onnx version is "+keras2onnx.__version__)
 
-def KerasToTensorflowModel(path_to_all=None, path_to_json= None, path_to_h5= None, prefix= None, name=None, numout=None, outdir=None):
+def KerasToTensorflowModel(path_to_all=None, job= None, path_to_json= None, path_to_h5= None, prefix= None, name=None, numout=None, outdir=None):
 
     if path_to_all is not None:
         outdir = os.path.join(path_to_all,'keras_tf_onnx_models/')
@@ -42,63 +42,96 @@ def KerasToTensorflowModel(path_to_all=None, path_to_json= None, path_to_h5= Non
     # Import model and weights #
     with open(path_to_json,"r") as f:
         keras_model_json = f.read()
-        #keras_model = model_from_json(keras_model_json, custom_objects =  {'PreprocessLayer': PreprocessLayer})
-        keras_model  = model_from_json(keras_model_json, custom_objects={name:getattr(Operations,name) for name in dir(Operations) if name.startswith('op')})
     
+    #keras_model = model_from_json(keras_model_json, custom_objects =  {'PreprocessLayer': PreprocessLayer})
+    keras_model  = model_from_json(keras_model_json, custom_objects={name:getattr(Operations,name) for name in dir(Operations) if name.startswith('op')})
+    
+    #keras_model_h5 = tf.keras.models.load_model( path_to_h5)
     keras_model.load_weights(path_to_h5)
-    #keras_model_ = tf.keras.models.load_model( path_to_h5)
-    try: 
-        print( os.path.join(outdir, suffix+".onnx") )
-        onnx_model  = keras2onnx.convert_keras(keras_model, keras_model.name)
-        keras2onnx.save_model(onnx_model, os.path.join(outdir, suffix+".onnx"))
+    
+    if job =='k2onnx':
+        try: 
+            print( os.path.join(outdir, suffix+".onnx") )
+            onnx_model  = keras2onnx.convert_keras(keras_model, keras_model.name)
+            keras2onnx.save_model(onnx_model, os.path.join(outdir, suffix+".onnx"))
+            
+            #onnx_model  = keras2onnx.convert_keras(keras_model_, keras_model_.name)
+            #keras2onnx_ = open(s.path.join(outdir, suffix+".onnx"), "wb")
+            #keras2onnx_.write(onnx_model.SerializeToString())
+            #keras2onnx_.close()
+        except Exception as ex:
+            logger.exception(f"ERROR while saving to onnx:{ex}")
         
-        #onnx_model  = keras2onnx.convert_keras(keras_model_, keras_model_.name)
-        #keras2onnx_ = open(s.path.join(outdir, suffix+".onnx"), "wb")
-        #keras2onnx_.write(onnx_model.SerializeToString())
-        #keras2onnx_.close()
-    except Exception as ex:
-        logger.exception(f"ERROR while saving to onnx:{ex}")
+        import onnx 
+        onnxmodel = onnx.load(os.path.join(outdir, suffix+".onnx"))
+        output    = [node.name for node in onnxmodel.graph.output]
     
-    import onnx 
-    onnxmodel = onnx.load(os.path.join(outdir, suffix+".onnx"))
-    output    = [node.name for node in onnxmodel.graph.output]
-
-    input_all = [node.name for node in onnxmodel.graph.input]
-    input_initializer =  [node.name for node in onnx_model.graph.initializer]
-    net_feed_input = list(set(input_all)  - set(input_initializer))
-
-    print('ONNX Inputs :', net_feed_input)
-    print('ONNX Outputs:', output)
-
-    # Alias the outputs in the model - this sometimes makes them easier to access in TF
-    numout = numout+".pb"
-    K.set_learning_phase(0)
-    pred = [None]*numout
-    pred_node_names = [None]*numout
-    for i in range(numout):
-        pred_node_names[i] = prefix+'_'+ suffix +'_'+str(i)
-        pred[i] = tf.identity(keras_model.output[i], name=pred_node_names[i])
-    print('Output nodes names are: ', pred_node_names)
-
-    sess = K.get_session()
+        input_all = [node.name for node in onnxmodel.graph.input]
+        input_initializer =  [node.name for node in onnx_model.graph.initializer]
+        net_feed_input = list(set(input_all)  - set(input_initializer))
     
-    # Write the graph in human readable
-    f = 'graph_def_for_reference.pb.ascii'
-    tf.train.write_graph(sess.graph.as_graph_def(), outdir, f, as_text=True)
-    print('Saved the graph definition in ascii format at: ', os.path.join(outdir, f))
+        print('ONNX Inputs :', net_feed_input)
+        print('ONNX Outputs:', output)
+    
+    else:
+        # Alias the outputs in the model - this sometimes makes them easier to access in TF
+        #K.set_learning_phase(0)
+        #pred = [None]*numout
+        #pred_node_names = [None]*numout
+        #for i in range(numout):
+        #    pred_node_names[i] = prefix+'_'+ suffix +'_'+str(i)
+        #    pred[i] = tf.identity(keras_model.output[i], name=pred_node_names[i])
+        #print('Output nodes names are: ', pred_node_names)
+    
+        txtfile = os.path.join(outdir,suffix+'_inputs.txt')
+        with open(txtfile,"w") as f:
+            for layer in keras_model.layers:
+                if 'InputLayer' in  str(type(layer)):
+                    f.write("{:50} -> {}\n".format(layer.name,str(layer.input_shape)))
+        print ("Saved input txt file as {}".format(txtfile))
+        
+        from tensorflow.python.keras.saving import saving_utils
+        from tensorflow.python.eager.def_function import Function
+        from tensorflow.python.eager.function import ConcreteFunction
 
-    # Write the graph in binary .pb file
-    constant_graph = graph_util.convert_variables_to_constants(sess, sess.graph.as_graph_def(), pred_node_names)
-    graph_io.write_graph(constant_graph, outdir, name, as_text=False)
-    print('Saved the constant graph (ready for inference) at: ', os.path.join(outdir, name))
+        assert isinstance(keras_model, tf.keras.Model)
+        learning_phase_orig = tf.keras.backend.learning_phase()
+        tf.keras.backend.set_learning_phase(False)
+        model_func = saving_utils.trace_model_call(keras_model)
+        if model_func.function_spec.arg_names and not model_func.input_signature:
+            raise ValueError("when model is a keras model callable accepting arguments, its "
+                            "input signature must be frozen by building the model")
+        model = model_func.get_concrete_function()
+        tf.keras.backend.set_learning_phase(learning_phase_orig)
+        
+        # convert variables to constants
+        assert isinstance(model, ConcreteFunction)
+
+        from tensorflow.python.framework import convert_to_constants
+        model = convert_to_constants.convert_variables_to_constants_v2(model)
+        graph = model.graph
+        
+        #sess = K.get_session()
+        # Write the graph in human readable
+        #f = 'graph_def_for_reference.pb.ascii'
+        #tf.train.write_graph(sess.graph.as_graph_def(), outdir, f, as_text=True)
+        #print('Saved the graph definition in ascii format at: ', os.path.join(outdir, f))
+    
+        # Write the graph in binary .pb file
+        #constant_graph = graph_util.convert_variables_to_constants(sess, sess.graph.as_graph_def(), pred_node_names)
+        graph_io.write_graph(graph, outdir, suffix+".pb", as_text=False)
+        graph_io.write_graph(graph, outdir, suffix+".pb.txt", as_text=True)
+        print('Saved the constant graph (ready for inference) at: ', os.path.join(outdir, suffix+".pb"))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', '-p', required=True, type=str, help='path where I an get the model/*_isbest_model/*.json && *.h5')
+    parser.add_argument('--outdir','-o', dest='outdir', required=False, default='./keras_tf_onnx_models', help='The directory to place the output files - default("./keras_tf_onnx_models")')
+    parser.add_argument('--job', required=False, type=str, choices=['k2tf', 'k2onnx'], help='What do you want: Keras -> TF or Keras -> ONNX')
+    
     parser.add_argument('--json',required=False, type=str,help='The json model file you wish to convert to .pb')
     parser.add_argument('--h5',required=False, type=str,help='The h5 model model weights file you wish to convert to .pb **do not use _full.h5**')
     parser.add_argument('--numout', type=int, required=False, default=3, help='The number of outputs in the model. default [DY, TT, ZA ] 3 nodes output')
-    parser.add_argument('--outdir','-o', dest='outdir', required=False, default='./keras_tf_onnx_models', help='The directory to place the output files - default("./keras_tf_onnx_models")')
     parser.add_argument('--prefix',dest='prefix', required=False, default='k2tf', help='The prefix for the output aliasing - default("k2tf")')
     parser.add_argument('--name', required=False, default='best_model', help='The name of the resulting output graph - default("best_model.pb and best_model.onnx") (MUST NOT forget)')
     args = parser.parse_args()
@@ -107,6 +140,7 @@ if __name__ == '__main__':
         print(' sorry this is not gonna work , either provid --json and --h5 or --path/ to model/*_isbest_model ')
 
     KerasToTensorflowModel(path_to_all      = args.path, 
+                           job              = args.job,
                            path_to_json     = args.json,
                            path_to_h5       = args.h5,
                            prefix           = args.prefix,
