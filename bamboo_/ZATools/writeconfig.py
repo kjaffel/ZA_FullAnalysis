8e61264d0a5aabd74dd9bd0164f86dbcc904ca6a
kjaffel <khawla.jaffel@cern.ch>
2021-11-14 14:38:59 +0100

    fix nasty errors with merged das path

--- bamboo_/ZATools/writeconfig.py
+++ bamboo_/ZATools/writeconfig.py
@@ -5,39 +5,28 @@ import yaml
 import random
 import argparse, optparse
 import numpy as np
+import re
 from collections import defaultdict
 from faker import Factory
 fake = Factory.create()
 
 # TODO 
-# 1- sum dataset 
 # 2- finish xsc uncer checks for mc 
-
-def get_das_path(search, inf):
-    das_tomerge = []
-    with open(options.das, 'r') as inf:
-        for smp in inf:
-            path   = smp.split()[0]
-            smpNm = path.split('/')[1]
-            if smpNm == search:
-                das_tomerge.append(f"das:{path}")
-    if len(das_tomerge) !=1:
-        return das_tomerge, smpNm 
-    else:
-        return das_tomerge[0], smpNm
 
 def get_era_and_luminosity(smp=None, run=None, isdata=False):
     preVFPruns  = ["B", "C", "D", "E", "F"]
     postVFPruns = ["G", "H"]
-    prefix = '-preVFP' if any(x in run for x in preVFPruns) else('-postVFP')
-    lumi   = 19667.812849099 if prefix=='-preVFP' else( 16977.701784453)
-    if 'UL2016'in smp: return f'2016{prefix}', lumi, 0.012
-    elif 'UL2017'in smp: return '2017', 41529.152060112, 0.023
-    elif 'UL2018'in smp: return '2018', 59740.565201546, 0.025
-    elif 'RunIISummer20UL16NanoAODAPV' in smp or 'RunIISummer19UL16NanoAODAPV' in smp: return '2016-preVFP', 19667.812849099, 0.012
-    elif 'RunIISummer20UL16NanoAOD' in smp or 'RunIISummer19UL16NanoAODAPV' in smp: return '2016-postVFP', 16977.701784453, 0.012
-    elif 'RunIISummer20UL17' in smp or 'RunIISummer19UL17' in smp: return '2017', 41529.152060112, 0.023
-    elif 'RunIISummer20UL18' in smp or 'RunIISummer19UL18' in smp: return '2018', 59740.565201546, 0.025
+    if isdata:
+        prefix = '-preVFP' if any(x in run for x in preVFPruns) else('-postVFP')
+        lumi   = 19667.812849099 if prefix=='-preVFP' else( 16977.701784453)
+        if 'UL2016'in smp: return f'2016{prefix}', lumi, 0.012
+        elif 'UL2017'in smp: return '2017', 41529.152060112, 0.023
+        elif 'UL2018'in smp: return '2018', 59740.565201546, 0.025
+    else:
+        if 'RunIISummer20UL16NanoAODAPV' in smp or 'RunIISummer19UL16NanoAODAPV' in smp: return '2016-preVFP', 19667.812849099, 0.012
+        elif 'RunIISummer20UL16NanoAOD' in smp or 'RunIISummer19UL16NanoAOD' in smp: return '2016-postVFP', 16977.701784453, 0.012
+        elif 'RunIISummer20UL17' in smp or 'RunIISummer19UL17' in smp: return '2017', 41529.152060112, 0.023
+        elif 'RunIISummer20UL18' in smp or 'RunIISummer19UL18' in smp: return '2018', 59740.565201546, 0.025
 
 def mass_to_str(m):
     return str(m).replace('.','p')
@@ -116,45 +105,67 @@ def get_list_ofsystematics(eras):
     return sys
 
 def get_mcNmConvention_and_group(smpNm):
-    shortnames = {'DYJetsToLL_0J'   : ['DY', 4757.0,     0., 'Drell-Yan', "#0000FF",    8],
-                  'DYJetsToLL_1J'   : ['DY', 859.589402, 0., 'Drell-Yan', "#0000FF",    8],
-                  'DYJetsToLL_2J'   : ['DY', 361.4,      0., 'Drell-Yan', "#0000FF",    8],
-                  'TTHadronic'            : ['ttbar_FullHadronic', 0.,     0., 'tt Full Had.',  "#00ffc7",  7],
-                  'TTToSemiLeptonic'      : ['ttbar_SemiLeptonic', 831.76, 0., 'tt Semi Lept.', "#9370DB",  6],
-                  'TTTo2L2Nu'             : ['ttbar_FullLeptonic', 88.40,  0., 'tt Full Lept.', "#c4ffff",  5],
-                  'ST_tW_top_5f'          : ['ST', 34.91,  0., 'Single Top',    "#ffc800",  4],
-                  'ST_tW_antitop_5f'      : ['ST', 34.97,  0., 'Single Top',    "#ffc800",  4],
-                  'ST_tchannel_top_4f'    : ['ST', 113.3,  0., 'Single Top',    "#ffc800",  4],
-                  'ST_tchannel_antitop_4f': ['ST', 67.91,  0., 'Single Top',    "#ffc800",  4],
-                  'ST_schannel_4f'        : ['ST', 3.74,   0., 'Single Top',    "#ffc800",  4],
-                  'ZZTo2L2Nu'   : ['ZZ', 0.5644, 0.0002688, 'ZZ',   "#ff4800",  3],
-                  'ZZTo2L2Q'    : ['ZZ', 3.222, 0.004901,   'ZZ',   "#ff4800",  3],
-                  'ZZTo4L'      : ['ZZ', 1.256, 0.002271,   'ZZ',   "#ff4800",  3],
-                  'HZJ_HToWW_M125'          : ['SM', 0.7524,  0.003643, 'tth, Zh',  "#ff0038",  2],
-                  'ZH_HToBB_ZToLL_M125'     : ['SM', 0.5269,  0.003834, 'tth, Zh',  "#ff0038",  2],
-                  'ggZH_HToBB_ZToLL_M125'   : ['SM', 0.5638,  0.02855,  'tth, Zh',  "#ff0038",  2],
-                  'ggZH_HToBB_ZToNuNu_M125' : ['SM', 0.01373, 0.,       'tth, Zh',  "#ff0038",  2],
-                  'ttHTobb'                 : ['SM', 0.5638,  0.,       'tth, Zh',  "#ff0038",  2],
-                  'ttHToNonbb'              : ['SM', 0.5638,  0.,       'tth, Zh',  "#ff0038",  2],
-                  'WWToLNuQQ'       : ['others', 43.53,     0.,     'others',   "#ff8d58",  1],
-                  'WWTo2L2Nu'       : ['others', 10.48,     0.,     'others',   "#ff8d58",  1],
-                  'WZTo2L2Q'        : ['others', 5.606,     0.,     'others',   "#ff8d58",  1],
-                  'WZTo1L3Nu'       : ['others', 3.054,     0.,     'others',   "#ff8d58",  1],
-                  'WZTo1L1Nu2Q'     : ['others', 10.73,     0.,     'others',   "#ff8d58",  1],
-                  'WZTo3LNu'        : ['others', 4.43,      0.,     'others',   "#ff8d58",  1],
-                  'WWW_4F'          : ['others', 0.2086,    0.,     'others',   "#ff8d58",  1],
-                  'WWZ_4F'          : ['others', 0.1651,    0.,     'others',   "#ff8d58",  1],
-                  'WZZ'             : ['others', 0.05565,   0.,     'others',   "#ff8d58",  1],
-                  'ZZZ'             : ['others', 0.01398,   0.,     'others',   "#ff8d58",  1],
-                  'WJetsToLNu'      : ['others', 60430.0,   0.,     'others',   "#ff8d58",  1],
-                  'TTWJetsToQQ'     : ['others', 0.405,     0.,     'others',   "#ff8d58",  1],
-                  'TTWJetsToLNu'    : ['others', 0.2001,    0.,     'others',   "#ff8d58",  1],
-                  'TTZToQQ'         : ['others', 0.5297,    0.,     'others',   "#ff8d58",  1],
-                  'TTZToLLNuNu_M10' : ['others', 0.2529,    0.,     'others',   "#ff8d58",  1],
+    shortnames = {'DYJetsToLL_0J'   : ['DY', 4757.0,     0., 'Drell-Yan', '#0000FF',    8],
+                  'DYJetsToLL_1J'   : ['DY', 859.589402, 0., 'Drell-Yan', '#0000FF',    8],
+                  'DYJetsToLL_2J'   : ['DY', 361.4,      0., 'Drell-Yan', '#0000FF',    8],
+                  'TTHadronic'            : ['ttbar_FullHadronic', 0.,     0., 'tt Full Had.',  '#00ffc7',  7],
+                  'TTToSemiLeptonic'      : ['ttbar_SemiLeptonic', 831.76, 0., 'tt Semi Lept.', '#9370DB',  6],
+                  'TTTo2L2Nu'             : ['ttbar_FullLeptonic', 88.40,  0., 'tt Full Lept.', '#c4ffff',  5],
+                  'ST_tW_top_5f'          : ['ST', 34.91,  0., 'Single Top',    '#ffc800',  4],
+                  'ST_tW_antitop_5f'      : ['ST', 34.97,  0., 'Single Top',    '#ffc800',  4],
+                  'ST_tchannel_top_4f'    : ['ST', 113.3,  0., 'Single Top',    '#ffc800',  4],
+                  'ST_tchannel_antitop_4f': ['ST', 67.91,  0., 'Single Top',    '#ffc800',  4],
+                  'ST_schannel_4f'        : ['ST', 3.74,   0., 'Single Top',    '#ffc800',  4],
+                  'ZZTo2L2Nu'   : ['ZZ', 0.5644, 0.0002688, 'ZZ',   '#ff4800',  3],
+                  'ZZTo2L2Q'    : ['ZZ', 3.222, 0.004901,   'ZZ',   '#ff4800',  3],
+                  'ZZTo4L'      : ['ZZ', 1.256, 0.002271,   'ZZ',   '#ff4800',  3],
+                  'HZJ_HToWW_M125'          : ['SM', 0.7524,  0.003643, 'tth, Zh',  '#ff0038',  2],
+                  'ZH_HToBB_ZToLL_M125'     : ['SM', 0.5269,  0.003834, 'tth, Zh',  '#ff0038',  2],
+                  'ggZH_HToBB_ZToLL_M125'   : ['SM', 0.5638,  0.02855,  'tth, Zh',  '#ff0038',  2],
+                  'ggZH_HToBB_ZToNuNu_M125' : ['SM', 0.01373, 0.,       'tth, Zh',  '#ff0038',  2],
+                  'ttHTobb'                 : ['SM', 0.5638,  0.,       'tth, Zh',  '#ff0038',  2],
+                  'ttHToNonbb'              : ['SM', 0.5638,  0.,       'tth, Zh',  '#ff0038',  2],
+                  'WWToLNuQQ'       : ['others', 43.53,     0.,     'others',   '#ff8d58',  1],
+                  'WWTo2L2Nu'       : ['others', 10.48,     0.,     'others',   '#ff8d58',  1],
+                  'WZTo2L2Q'        : ['others', 5.606,     0.,     'others',   '#ff8d58',  1],
+                  'WZTo1L3Nu'       : ['others', 3.054,     0.,     'others',   '#ff8d58',  1],
+                  'WZTo1L1Nu2Q'     : ['others', 10.73,     0.,     'others',   '#ff8d58',  1],
+                  'WZTo3LNu'        : ['others', 4.43,      0.,     'others',   '#ff8d58',  1],
+                  'WWW_4F'          : ['others', 0.2086,    0.,     'others',   '#ff8d58',  1],
+                  'WWZ_4F'          : ['others', 0.1651,    0.,     'others',   '#ff8d58',  1],
+                  'WZZ'             : ['others', 0.05565,   0.,     'others',   '#ff8d58',  1],
+                  'ZZZ'             : ['others', 0.01398,   0.,     'others',   '#ff8d58',  1],
+                  'WJetsToLNu'      : ['others', 60430.0,   0.,     'others',   '#ff8d58',  1],
+                  'TTWJetsToQQ'     : ['others', 0.405,     0.,     'others',   '#ff8d58',  1],
+                  'TTWJetsToLNu'    : ['others', 0.2001,    0.,     'others',   '#ff8d58',  1],
+                  'TTZToQQ'         : ['others', 0.5297,    0.,     'others',   '#ff8d58',  1],
+                  'TTZToLLNuNu_M10' : ['others', 0.2529,    0.,     'others',   '#ff8d58',  1],
                   }
     for Nm, val in shortnames.items():
         if smpNm.replace('-','').startswith(Nm):
             return Nm , val[0], val[1], val[2], val[3], val[4], val[5] # name, xsc, uncer, legend, fill_color, order_of_group_in_plotit
+
+def get_das_path(inf, smp, search, era, run, isdata=False, isMC=False, issignal=False):
+    das_tomerge  = []
+    to_ignore    = []
+    with open(options.das, 'r') as inf:
+        for line in inf:
+            path   = line.split()[0]
+            
+            if isdata:
+                regex = re.compile(f"/{search}/Run{era.split('-')[0]}{run}-ver*", re.IGNORECASE)
+            else:
+                regex = re.compile(f"/{search}/*RunIISummer*{era.split('-')[0].split('20')[1]}NanoAOD*_ext*", re.IGNORECASE)
+            m = regex.search(path)
+            if m:
+                # this is an extension add to merge 
+                das_tomerge.append('das:{}'.format(path))
+                to_ignore.append( path)
+        
+    if das_tomerge:
+        return das_tomerge, to_ignore
+    else:
+        return 'das:{}'.format(smp), []
 
 def get_legend(process, comp, H, l, m_heavy, m_light, smpNm):
     return f"{process}-{comp}: M{H}-{mass_to_str(m_heavy)}_M{l}-{mass_to_str(m_light)}_tb-{mass_to_str(tb)}"
@@ -179,8 +190,11 @@ def lumi_block(inf):
     with open(options.das, 'r') as inf:
         eras = defaultdict()
         for smp in inf:
+            smp = smp.split()[0]
             run = smp.split('/')[2].split('-')[0][-1]
-            era, lumi, uncer = get_era_and_luminosity(smp, run)
+            smpNm = smp.split('/')[1]
+            isdata = True if smpNm in ['MuonEG', 'DoubleEG', 'EGamma', 'DoubleMuon', 'SingleMuon'] else(False)
+            era, lumi, uncer = get_era_and_luminosity(smp, run, isdata)
             if era not in eras.keys(): 
                 eras[era] = {}
                 eras[era]['lumi'] = lumi
@@ -190,7 +204,7 @@ def lumi_block(inf):
 def get_label(eras):
     # I am not gonna do more then this split : 2016 pre/-postVFP, 2017, 2018 
     #                                        or all combined : run2
-    if len (eras.keys()) ==2 and '2016' in eras.keys():
+    if len(eras.keys())==2 and ('2016-' in x for x in eras.keys() ):
         suffix = 'pre/-postVFP 2016'
     else:
         if len(eras.keys()) == 1 :
@@ -198,8 +212,9 @@ def get_label(eras):
         elif len(eras.keys()) == 4 :
             suffix = 'run2'
         else:
+            suffix = ''
             for k in eras.keys():
-                suffix  += ' {k},' 
+                suffix  += f' {k},' 
     plot_label = f'{suffix} ULegacy'
     return plot_label 
 
@@ -271,13 +286,15 @@ if __name__ == "__main__":
     
     eras = lumi_block(options.das)
     print( eras)
-    merged_daspath = []
     with open(options.das, 'r') as inf:
         with open(options.output, 'w+') as outf:
             isMC     = False
             isdata   = False
             issignal = False
+            
             groups = defaultdict()
+            merged_daspath = []
+            
             # =======================================================
             outf.write(f" tree: Events\n")
             outf.write(f" eras:\n")
@@ -285,8 +302,11 @@ if __name__ == "__main__":
                 outf.write(f"   '{era}':\n")
                 outf.write(f"     luminosity: {opt['lumi']} #pb \n")
                 outf.write(f"     luminosity-error: {opt['uncer']}\n")
+            outf.write("\n")
             # =======================================================
+            run = None
             for i, smp in enumerate(inf):
+                smp   = smp.split()[0]
                 smpNm = smp.split('/')[1]
                 if "HToZATo2L2B" in smp or "AToZHTo2L2B" in smp : 
                     isMC     = False
@@ -295,7 +315,7 @@ if __name__ == "__main__":
                     mode     = ('AToZH' if smpNm.startswith('AToZH') else 'HToZA')
                     comp     = 'nlo' if 'amcatnlo' in smp else 'lo'
                     process  = 'ggH' if smpNm.startswith('GluGlu') else('bbH')
-                elif smpNm in ['MuonEG', 'DoubleEGamma', 'EGamma', 'DoubleMuon', 'SingleMuon']: 
+                elif smpNm in ['MuonEG', 'DoubleEG', 'EGamma', 'DoubleMuon', 'SingleMuon']: 
                     isMC     = False
                     isdata   = True
                     issignal = False
@@ -304,14 +324,13 @@ if __name__ == "__main__":
                     isdata   = False
                     issignal = False
                 
-                das__path, to_ignore = get_das_path( smpNm, options.das)
-                if smpNm in merged_daspath:
-                    continue
-                merged_daspath.append( to_ignore)
                 color  = fake.hex_color()
                 #color = '#%06x' % random.randint(0, 0xFFFFFF)
-                run = smp.split('/')[2].split('-')[0][-1]
+                
+                if isdata:
+                    run = smp.split('/')[2].split('-')[0][-1]
                 era, lumi, uncer = get_era_and_luminosity(smp, run, isdata)
+                era_ = era.split('-')
                 
                 if issignal:
                     benchmarks = loadSushiInfos(len(smpNm),f"{base}/list_benchmarks_{process}_{comp}_{mode}_datasetnames.txt")
@@ -320,27 +339,36 @@ if __name__ == "__main__":
                     
                     arrs = np.concatenate((benchmarks, fullsim, all_))
                     H, l, mHeavy, mlight, tb, xsc, xsc_err, br_HeavytoZlight, br_lighttobb = get_xsc_br_fromSushi(smpNm, arrs)
-                    Nm      = smpNm.replace('-','_')
+                    Nm      = smpNm.replace('-','_')+f'_{era_[1]}'
                     br      = br_HeavytoZlight *  br_lighttobb
                     leg     = get_legend(process, comp, H, l, mHeavy, mlight, smpNm)
                     split   = 1 
+                    search  = smpNm
                     details = f'{H} -> Z{l} : {br_HeavytoZlight} * {l} -> bb : {br_lighttobb}'
                 elif isdata:
-                    era_ = era.split('-')
                     Nm = f'{smpNm}_UL{era_[0]}{run}_{era_[1]}'
                     run_range = run2_ranges[era][run]
                     cert = certification[era.split('-')[0]] # FIXME make sure that this assuption is correct : means the certefication is the same for pre/post VFP
-                    split = 1
+                    split  = 1
+                    search = smpNm
                 elif isMC:
                     Nm, group, xsc, uncer, legend, fill_color, order = get_mcNmConvention_and_group(smpNm)
+                    Nm = Nm + f'_{era_[1]}'
                     split = 4
+                    search = smpNm
                     if group not in groups.keys(): 
                         groups[group] = {}
                         groups[group]['legend'] = legend
                         groups[group]['fill_color'] = fill_color
                         groups[group]['order'] = order
+                
+                if str(smp) in merged_daspath:
+                    continue
+                das__path, to_ignore = get_das_path(options.das, smp, search, era, run, isdata, isMC, issignal)
+                merged_daspath.extend( to_ignore)
+                
                 outf.write(f"  {Nm}:\n")
-                outf.write(f"    db: {das__path}\n")
+                outf.write(f'    db: {das__path}\n'.replace("'" , ""))
                 outf.write(f"    files: dascache/nanov9/{Nm}.dat\n")
                 outf.write(f"    split: {split}\n")
                 outf.write(f"    era: '{era}'\n")
@@ -388,7 +416,7 @@ if __name__ == "__main__":
             outf.write("      legend: Signal\n")
             for gp, opt in groups.items():
                 outf.write(f"    {gp}:\n")
-                outf.write(f"      fill-color: {opt['fill_color']}\n")
+                outf.write(f"      fill-color: '{opt['fill_color']}'\n")
                 outf.write(f"      legend: {opt['legend']}\n")
                 outf.write(f"      order: {opt['order']}\n")
             outf.write("  plotdefaults:\n")
