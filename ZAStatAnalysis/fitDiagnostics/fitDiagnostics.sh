a737860f9702a9c55807b0856ed329c8082eae3e
kjaffel <khawla.jaffel@cern.ch>
2021-12-07 14:25:05 +0100

    update fit diagnostic script

--- ZAStatAnalysis/fitDiagnostics/fitDiagnostics.sh
+++ ZAStatAnalysis/fitDiagnostics/fitDiagnostics.sh
@@ -2,32 +2,35 @@
 # https://twiki.cern.ch/twiki/bin/viewauth/CMS/HiggsWG/HiggsPAGPreapprovalChecks
 # https://cms-analysis.github.io/HiggsAnalysis-CombinedLimit/part3/nonstandard/#fit-parameter-uncertainties
 
-val=${1:?"Missing arg 1 for --expectSignal => expected 1 for S+B and 0 for B-Only"}
+val=${1:?"Missing arg 1 : --expectSignal => expected 1 for S+B and 0 for B-Only"}
+dir=${2:?"Missing arg 2 : --dir          => path_to_cards_dir/<options.method>-limits/<options.mode>/"}
+mode=${3:?"Missing arg 3: --mode         => choices are [mjj_vs_mlljj, mjj_and_mlljj, postfit, mjj, mlljj, ellipse, dnn] needed for fit diagnostics 
+                          and the path to dir will be :: path_to_cards_dir/fit/<options.mode>/"}
 
-#dir="../Cards_part0_exp_sx100_forImpacts/ZA/ellipse/limits/"
-dir="../ul__combinedlimits/ul2016_cards__ver1/dnn/limits/"
-cd $dir
+pushd $dir
 
-channel="MuMu"
+fit_diagDIR='FitDiagnostics/expectSignal'$val
+if [[ ! -d "$fit_diagDIR" ]]; then
+    mkdir -p $fit_diagDIR
+fi
+
+pushd fit/$mode
 
 for f in *; do
     if [[ -d $f ]]; then
         pushd $f
         echo "Accessing " $f "..."
-        #HToZATo2L2B_ElEl_dnn_ggH_resolved_MH_300_MA_50.dat
-        datacard=`find -name "HToZATo2L2B_$channel*.dat"`
-        echo $datacard
-        output="${datacard/.dat/.txt}"
         
-        fit_diagDIR='FitDiagnostics/expectSignal'$val
-        if [[ ! -d "$fit_diagDIR" ]]; then
-            mkdir -p $fit_diagDIR
-        fi
-        pwd 
-        #combine -M FitDiagnostics -m 125 -t -1 --expectSignal $val --toysFrequentist --rMin -20 --rMax 20 $datacard >> "$fit_diagDIR/$output"
-        combine -M FitDiagnostics -m 125 -t -1 --expectSignal $val $datacard >> "$fit_diagDIR/$output"
-        python ${CMSSW_BASE}/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py -a fitDiagnostics.root -g plots.root >> "$fit_diagDIR/$output"
-    
+        datacards=`find -name "*.dat"`
+        for p in $datacards; do
+            datacard=${p##*/}
+            output=${datacard/.dat/.log}
+            pwd 
+           #combine -M FitDiagnostics -m 125 --rMin -20 --rMax 20 -t -1 --expectSignal $val $datacard --toysFrequentist >> $fit_diagDIR/$output
+            combine -M FitDiagnostics -m 125 --rMin -20 --rMax 20 -t -1 --expectSignal $val $datacard >>  $CMSSW_BASE/$dir/$fit_diagDIR/$output
+            python ${CMSSW_BASE}/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py -a fitDiagnostics.root -g plots.root >>  $CMSSW_BASE/$dir/$fit_diagDIR/$output
+        done
         popd
     fi
 done
+popd
