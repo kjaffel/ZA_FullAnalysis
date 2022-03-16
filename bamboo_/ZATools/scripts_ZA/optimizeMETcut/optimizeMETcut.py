7a375609167ff6083c6f68202a2ec3461f23565b
kjaffel <khawla.jaffel@cern.ch>
2022-03-16 15:25:07 +0100

    update script for ul to check met cut for the different categories

--- bamboo_/ZATools/scripts_ZA/optimizeMETcut/optimizeMETcut.py
+++ bamboo_/ZATools/scripts_ZA/optimizeMETcut/optimizeMETcut.py
@@ -6,21 +6,28 @@
 # Then vary the met cut bin by bin and get the integral of the backrounds histo and of the signal histo and compute S/sqrt(B)
 
 #! /bin/env python
-import sys, os, json
-import copy
-import datetime
-import subprocess
-import numpy as np
+import sys, os
 import math
 import glob
 import ROOT
+import yaml
+import numpy as np
+
+ROOT.gROOT.SetBatch(True)
 from ROOT import TCanvas, TPad, TLine, TH1F
+from faker import Factory
+fake = Factory.create()
 
-import argparse
-from random import randint
+sys.path.append('/home/ucl/cp3/kjaffel/bamboodev/ZA_FullAnalysis/ZAStatAnalysis/')
+import Constants as Constants
 
+wheel  = [ROOT.kTeal,ROOT.kBlue, ROOT.kAzure, ROOT.kViolet, ROOT.kTeal, ROOT.kYellow, ROOT.kOrange, ROOT.kGreen, ROOT.kMagenta, ROOT.kCyan, ROOT.kYellow]
+colors = []
+for i in range(1, 10):
+    for c in wheel:
+        colors.append(c+i)
 
-def getHisto(path, isBkg, prefix, reg, tagger, cat):
+def getHisto(era, Cfg, path, isBkg, prefix, reg, tagger, wp, cat, process):
 
     _files = set()
     histo_met = TH1F("histo_met", "histo_met", 60, 0, 600)
@@ -28,105 +35,179 @@ def getHisto(path, isBkg, prefix, reg, tagger, cat):
     integral=0
     for i, filename in enumerate(glob.glob(os.path.join(path, '*.root'))):
         split_filename = filename.split('/')
-        if str(split_filename[-1]).startswith('__skeleton__'):
+        smp = split_filename[-1]
+        if smp.startswith('__skeleton__'):
             continue
+
         if isBkg:
-            if str(split_filename[-1]).startswith("DoubleMuon") or str(split_filename[-1]).startswith("DoubleEG") or str(split_filename[-1]).startswith("MuonEG") or str(split_filename[-1]).startswith("HToZA"):
+            if smp.startswith("DoubleMuon") or smp.startswith("DoubleEG") or smp.startswith("MuonEG") or smp.startswith("HToZA") or smp.startswith("GluGluToHToZA"):
                 continue
+            smpScale = 1.
         else:
-            if not str(split_filename[-1]) == prefix:
+            if not smp.startswith(prefix):
                 continue
+            br = Cfg['files'][smp]["Branching-ratio"]
+            smpScale = br
+       
+        if not smp in Cfg['files'].keys():
+            continue
+        year   = Cfg['files'][smp]["era"]
+        lumi   = Cfg["configuration"]["luminosity"][year]
+        xsc    = Cfg['files'][smp]["cross-section"]
+        genevt = Cfg['files'][smp]["generated-events"]
+        
+        smpScale *= ( lumi* xsc )/ genevt
+
         f = ROOT.TFile.Open(filename)
         _files.add(f)
         for j, key in enumerate(f.GetListOfKeys()):
             cl = ROOT.gROOT.GetClass(key.GetClassName())
-            if key.ReadObj().GetName() == "xycorrmet_pt_{0}_{1}_hZA_lljj_{2}M".format(reg, cat, tagger):
+            if key.ReadObj().GetName() == "xycorrmet_pt_{}_{}_hZA_lljj_{}{}_{}".format(reg, cat, tagger, wp, process):
                 key.ReadObj().SetDirectory(0)
                 integral = integral + key.ReadObj().Integral()
                 histo_met.Add(key.ReadObj(), 1)
-        histo_met.SetDirectory(0)  
+        histo_met.SetDirectory(0)
+        histo_met.Scale(smpScale)
     return histo_met
 
 
-def main():
-
-    global lumi
-    lumi = 35921.875594646
-    #path = '/home/ucl/cp3/kjaffel/scratch/ZAFullAnalysis/2016Results/version20_02_19/ellipses_metcut_bjets_/results'
-    #path = '/home/ucl/cp3/kjaffel/scratch/ZAFullAnalysis/2016Results/version20_04_01/plots_toOptimizeMETCut/'
-    path = '/home/ucl/cp3/kjaffel/scratch/ZAFullAnalysis/2016Results/ver.20_04_01/plots_toOptimizeMETCut/'
-    category = ["MuMu", "ElEl", "MuEl"]
+def gethistNm(path, process):
+    look_for = "GluGluToHToZATo2L2B" if process == "gg_fusion" else "HToZATo2L2B"
     prefix = []
     for i, filename in enumerate(glob.glob(os.path.join(path, '*.root'))):
-        #print("filename:", filename)
         split_filename = filename.split('/')
-        if str(split_filename[-1]).startswith("HToZATo2L2B"):
-            prefix.append(split_filename[-1])
-    
-    c = []
-    legend = []
-    taggers=["DeepCSV"]
-    
-    for reg in ['resolved']:#, 'boosted']:
-        if reg=='resovled':
-            taggers.append("DeepFlavour")
-        for tagger in taggers:
+        if str(split_filename[-1]).startswith(look_for):
+            fnm = split_filename[-1].replace('preVFP.root','').replace('postVFP.root', '')
+            if not fnm in prefix:
+                prefix.append(fnm)
+    return prefix
+
+def optimizeMETcut(era, Cfg, path):
+
+    category = ["MuMu", "ElEl"]#, "MuEl"]
+    workingPoint = "M"
+    yaxis_max = {"gg_fusion":{
+                    "resolved":  5.,
+                    "boosted" :  5. },
+                "bb_associatedProduction":{
+                    "resolved" : 5.,
+                    "boosted"  : 5. }
+                }
+    for process in ['gg_fusion', 'bb_associatedProduction']:
+        prefix = gethistNm(path, process)
+   
+        for reg, tagger in {'resolved':'DeepFlavour', 
+                            'boosted' : 'DeepCSV'}.items():
+            c = []
+            legend = []
             for k, cat in enumerate(category):
     
-                legend.append(ROOT.TLegend(0.85,0.55,0.95,0.95))
-                legend[k].SetHeader("{0} category".format(cat))
-                histo_met_bkg = getHisto(path, isBkg=True, prefix="", reg=reg, tagger=tagger, cat=cat)
+                legend.append(ROOT.TLegend(0.5,0.89,0.6,0.62))
+                legend[k].SetTextSize(0.025)
+                legend[k].SetBorderSize(0)
+                
+                histo_met_bkg = getHisto(era, Cfg, path, isBkg=True, prefix="", reg=reg, tagger=tagger, wp= workingPoint, cat=cat, process=process)
 
                 i=0
                 graphs = []
+                masspoints = []
                 for pref in prefix:
                     split_prefix = pref.split("_")
                     if "2000" in pref or "3000" in pref:
                         continue
                     significance = []
                     xAxis = []
+                    toSkip=False
+                    print( "working on ::", pref, process, reg, cat)
                     for i in range(0, 201, 5):
                         histo_met_bkg.GetXaxis().SetRangeUser(11, i)
                         histo_met_bkg.GetYaxis().SetRangeUser(0, 1.0)
                         #Check why for different i, the integral is the same
                         histo_met_bkg.SetDirectory(0)
-                        histo_met_sig = getHisto(path, isBkg=False, prefix=pref, reg=reg, tagger=tagger, cat=cat)
+                        histo_met_sig = getHisto(era, Cfg, path, isBkg=False, prefix=pref, reg=reg, tagger=tagger, wp= workingPoint, cat=cat, process=process)
                         histo_met_sig.GetXaxis().SetRangeUser(11, i)
                         histo_met_sig.GetYaxis().SetRangeUser(0, 1.0)
                         histo_met_sig.SetDirectory(0)
+                        
                         #significance = 2*(SQRT(S+B)-SQRT(B))
                         #signif = 2*(math.sqrt(histo_met_sig.Integral() + histo_met_bkg.Integral()) - math.sqrt(histo_met_bkg.Integral()))
+                        
                         S = histo_met_sig.Integral()
                         B = histo_met_bkg.Integral()
-                        #print ("signal Integral", S, "Background Integral:" , B)
+                        eq = 2*( (S+B)*math.log(1+S/B) -S ) 
+                        
                         signif = math.sqrt(2*( (S+B)*math.log(1+S/B) -S ))
-                        #print ("signal: ", pref, "   MET_pT: ", i, "   significance: ", signif)
+                        if eq < 0. or signif > 5.:
+                            toSkip=True
+                            continue
+                        print ("\t\tMET_pT: ", i, "Integral (S, B) = ", (S, B), "significance =", signif)
+                        
                         significance.append(float(signif))
                         xAxis.append(float(i))
     
-                    significance_array = np.array(significance)
-                    xAxis_array = np.array(xAxis)
-                    graph = ROOT.TGraph(int(40), xAxis_array, significance_array)
-                    graph.SetName(split_prefix[1]+"_"+split_prefix[2])
-                    graphs.append(graph)
+                    if not toSkip:
+                        significance_array = np.array(significance)
+                        xAxis_array = np.array(xAxis)
+                        graph = ROOT.TGraph(int(40), xAxis_array, significance_array)
+                        graph.SetName(pref.replace('.root', ''))
+                        graphs.append(graph)
+                    
+                        mH = split_prefix[2].replace('p00', '')
+                        mA = split_prefix[4].replace('p00', '')
+                        masspoints.append('(MH, MA)= (%s, %s) GeV'%(mH, mA))
+                    print( "===="*20)
         
                 c.append(TCanvas("c{0}".format(k),"c{0}".format(k),800,600))
                 #c[k].DrawFrame(40,0.0012,160,0.02).SetTitle("Significance vs MET cut; MET cut (GeV); 2(#sqrt{S+B} - #sqrt{B})")
                 #c[k].DrawFrame(0,0.0012,160,0.02).SetTitle("Significance vs MET cut; MET cut (GeV); #sqrt{2((S+B)ln(1+S/B)-S)}")
-                c[k].DrawFrame(0,0,210,3.5).SetTitle("Significance vs MET cut; MET cut (GeV); #sqrt{2((S+B)ln(1+S/B)-S)}")
+                c[k].DrawFrame(0,0,210,yaxis_max[process][reg]).SetTitle("; MET cut (GeV); Significance #sqrt{2((S+B)ln(1+S/B)-S)}")
         
-                colors= [ROOT.kRed, ROOT.kTeal-5, ROOT.kYellow, ROOT.kRed-7, ROOT.kOrange, ROOT.kOrange-3, ROOT.kOrange+2, ROOT.kGreen-4, ROOT.kMagenta-2, ROOT.kMagenta-6, ROOT.kMagenta-9, ROOT.kGreen, ROOT.kGreen+3, ROOT.kGreen-2, ROOT.kGreen-5, ROOT.kCyan+1, ROOT.kCyan+3, ROOT.kBlue, ROOT.kBlue+2, ROOT.kBlue-9, ROOT.kYellow+3 ]
-                for gr, color in zip(graphs, colors):
-                    legend[k].AddEntry(gr, gr.GetName(), "l")
+                for i, (gr, Nm) in enumerate(zip(graphs, masspoints)):
+                    legend[k].AddEntry(gr, Nm, "l")
                     gr.Draw("*L")
-                    gr.SetMarkerColor(color)
-                    gr.SetLineColor(color)
-                legend[k].Draw()
-                c[k].cd() 
-    
-                c[k].SaveAs("optimizeMETcut_{0}_{1}_{2}M.png".format(reg, cat.lower(), tagger.lower()))
-                #del c1
-if __name__ == "__main__":
-    ROOT.gROOT.SetBatch(True)
-    main()
+                    gr.SetMarkerStyle(ROOT.kFullSquare)
+                    gr.SetMarkerColor(colors[i])
+                    gr.SetLineColor(colors[i])
+                
+                legend[k].Draw("*L")
+                
+                t = c[k].GetTopMargin()
+                r = c[k].GetRightMargin()
+                l = c[k].GetLeftMargin()
+                lumiTextOffset   = 0.2
 
+                latex = ROOT.TLatex()
+                latex.SetNDC()
+                latex.SetTextAngle(0)
+                latex.SetTextColor(ROOT.kBlack)
+                
+                lumiText = "%s fb^{-1} (13 TeV)" %format(Constants.getLuminosity(era)/1000.,'.2f')
+                lumi = latex.DrawLatex(0.7,1-t+lumiTextOffset*t,lumiText)
+                lumi.SetNDC()
+                lumi.SetTextFont(40)
+                lumi.SetTextSize(0.03)
+                lumi.Draw("same")
+                
+                cms_text = latex.DrawLatex(0.12, 1-t+lumiTextOffset*t, "CMS Preliminary")
+                cms_text.SetNDC()
+                cms_text.SetTextFont(40)
+                cms_text.SetTextSize(0.03)
+                cms_text.Draw("same")
+                
+                c[k].cd() 
+                c[k].SaveAs("ul{}/optimizeMETcut_{}_{}_{}_{}{}.png".format(era, process, reg, cat, tagger, workingPoint))
+                c[k].SaveAs("ul{}/optimizeMETcut_{}_{}_{}_{}{}.pdf".format(era, process, reg, cat, tagger, workingPoint))
+                #del c
+
+if __name__ == "__main__":
+    #path = '/home/ucl/cp3/kjaffel/scratch/ZAFullAnalysis/2016Results/version20_02_19/ellipses_metcut_bjets_/results'
+    #path = '/home/ucl/cp3/kjaffel/scratch/ZAFullAnalysis/2016Results/version20_04_01/plots_toOptimizeMETCut/'
+    #path = '/home/ucl/cp3/kjaffel/scratch/ZAFullAnalysis/2016Results/ver.20_04_01/plots_toOptimizeMETCut/'
+    #path  = '/home/ucl/cp3/kjaffel/bamboodev/ZA_FullAnalysis/bamboo_/run2Ulegay_results/ul_2017__ver3/'
+    path  = '/home/ucl/cp3/kjaffel/bamboodev/ZA_FullAnalysis/bamboo_/run2Ulegay_results/ul_2016__ver16/'
+    era   = 2016
+    
+    with open(os.path.join(path, 'plots.yml')) as _f:
+        Cfg = yaml.load(_f, Loader=yaml.FullLoader)
+
+    optimizeMETcut(era, Cfg, os.path.join(path, 'results/'))
