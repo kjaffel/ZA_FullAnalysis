5e0835db5b3abddee8821c2d460d0ebe77157d44
kjaffel <khawla.jaffel@cern.ch>
2022-03-28 19:08:22 +0200

    update bjet energy regression

--- bamboo_/ZATools/compareBJetEnergyRegression.py
+++ bamboo_/ZATools/compareBJetEnergyRegression.py
@@ -1,25 +1,31 @@
 import os
 import sys
 import glob
+import collections 
 import argparse
 import copy
 import yaml
 import ROOT
+sys.path.append('/home/ucl/cp3/kjaffel/bamboodev/ZA_FullAnalysis/ZAStatAnalysis/')
+import Constants as Constants
 ROOT.gROOT.SetBatch(True)
 ROOT.gStyle.SetOptStat(0)
 
-class StudyHighPileupRegions():
-    def __init__(self, variable, variable_name, output, plot_data, plot_MC, plot_signal):
-        self.outname = "2017/"+output
-        self.era     = '2017'
-        self.variable = variable
-        self.variable_name = variable_name
-        self.data_hist_dict = {}
-        self.signal_hist_dict = {}
-        self.MC_hist_dict = {}
-        self.plot_data = plot_data
-        self.plot_MC = plot_MC
-        self.plot_signal = plot_signal
+class BJetEnergyRegression():
+    def __init__(self, era, process, channel, variable, variable_name, output, plot_data, plot_MC, plot_signal, plot_ratio):
+        self.outname             = output
+        self.era                 = era
+        self.process             = process
+        self.variable            = variable
+        self.variable_name       = variable_name
+        self.data_hist_dict      = {}
+        self.signal_hist_dict    = {}
+        self.MC_hist_dict        = {}
+        self.plot_data           = plot_data
+        self.plot_MC             = plot_MC
+        self.plot_signal         = plot_signal
+        self.plot_ratio          = plot_ratio
+        self.channel             = channel
 
         self.extractAllInformation()
         self.processHistograms()
@@ -40,7 +46,6 @@ class StudyHighPileupRegions():
         path_file = os.path.abspath(os.path.join(directory, 'results','*root'))
         for f in glob.glob(path_file):
             name = os.path.basename(f)
-            #print ("Looking at %s"%name)
             # Check if in YAML #
             if name not in yaml_dict['samples'].keys():
                 print ("[WARNING] File %s will be ignored"%(name))
@@ -49,16 +54,14 @@ class StudyHighPileupRegions():
             h = self.getHistogram(f,histogram)
             hist_dict[name] =h 
 
-        #print ('hist_dict', hist_dict)
         yaml_dict.update({'histograms':hist_dict})
-
         return yaml_dict
 
 
     def loadYaml(self,yaml_path,histogram):
-        # Parse YAML #
         with open(yaml_path,"r") as handle:
             full_dict = yaml.load(handle,Loader=yaml.FullLoader)
+        
         # Get Lumi per era #
         lumi_dict = full_dict["configuration"]["luminosity"]
         
@@ -72,10 +75,8 @@ class StudyHighPileupRegions():
 
         # Get data per sample #
         sample_dict = {}
-        try: 
-            info_to_keep = ['cross-section','generated-events','group','type','era', 'Branching-ratio']
-        except:
-            info_to_keep = ['cross-section','generated-events','group','type','era']
+        info_to_keep = ['cross-section','generated-events','group','type','era', 'Branching-ratio']
+
         for sample,data in full_dict['files'].items():
             sample_dict[sample] = {k:data[k] for k in data.keys() & info_to_keep}
 
@@ -83,7 +84,6 @@ class StudyHighPileupRegions():
             
     def getHistogram(self,rootfile,histname):
         f = ROOT.TFile(rootfile)
-        #print (rootfile)
         if f.GetListOfKeys().Contains(histname):
             h = copy.deepcopy(f.Get(histname))
         else:
@@ -94,11 +94,9 @@ class StudyHighPileupRegions():
 
     def processHistograms(self):
         colors = [221, 213, 2, ROOT.kRed, 9, ROOT.kCyan, 69, 90, 208] 
-        MCcolors = [ROOT.kCyan, ROOT.kRed] 
-        import collections 
+        MCcolors = [ROOT.kCyan, ROOT.kRed, 221] 
         
         for i,(key, val) in enumerate(self.info_dict.items()):
-            print ('key:', key)
             samp_dict=collections.defaultdict(dict)
             for smp, val2 in val['samples'].items():
                 if self.plot_data:
@@ -108,21 +106,28 @@ class StudyHighPileupRegions():
                     if not str(smp).startswith("TTTo2L2Nu") and not str(smp).startswith("TTHadronic") and not str(smp).startswith("TTToSemiLeptonic") and not str(smp).startswith("ST_"):
                             continue
                 if self.plot_signal:
-                    if not str(smp).startswith("HToZATo2L2B_MH-500_MA-300") :
-                        continue
-                
+                    if self.process == 'gg_fusion':
+                        if not (str(smp).startswith("GluGluToHToZATo2L2B_MH_500p00_MA_300p00") or str(smp).startswith("HToZATo2L2B_MH-500_MA-300")):  # 2nd for old signal samples 
+                            continue
+                    else:
+                        if not str(smp).startswith("HToZATo2L2B_MH_500p00_MA_400p00_"):
+                            continue
                 samp_dict[smp]= val2
-                print( samp_dict[smp] , smp ) 
-            data_hist = None
-            MC_hist = None
+            
+            data_hist   = None
+            MC_hist     = None
             signal_hist = None
+            
             hist_dict = val['histograms'] 
             lumi_dict = val['luminosity'] 
             plot_dict = val['plot_options'] 
             #samp_dict = val['samples']
             
-            #print ( samp_dict)
             for sample, data_dict in samp_dict.items():
+                if 'postVFP' in sample: era_ = '2016-postVFP'
+                elif 'preVFP' in sample: era_ = '2016-preVFP'
+                else: era_ = self.era
+
                 h = hist_dict[sample]
                 if h is None:
                     continue
@@ -131,44 +136,44 @@ class StudyHighPileupRegions():
                 if MC_hist is None:
                     MC_hist = ROOT.TH1F(self.variable_name,self.variable_name,h.GetNbinsX(),h.GetBinLowEdge(1),h.GetBinLowEdge(h.GetNbinsX())+h.GetBinWidth(h.GetNbinsX()))
                 if signal_hist is None:
-                    signal_hist = ROOT.TH1F(self.variable_name,self.variable_name,h.GetNbinsX(),h.GetBinLowEdge(1),h.GetBinLowEdge(h.GetNbinsX())+h.GetBinWidth(h.GetNbinsX()))
+                    signal_hist = ROOT.TH1F(self.variable_name,'',h.GetNbinsX(),h.GetBinLowEdge(1),h.GetBinLowEdge(h.GetNbinsX())+h.GetBinWidth(h.GetNbinsX()))
 
                 if data_dict['type'] == 'data':
                     data_hist.Add(h)
                 elif data_dict['type'] == 'mc':
-                    factor = data_dict["cross-section"]*lumi_dict[self.era]/data_dict["generated-events"]
+                    factor = data_dict["cross-section"]*lumi_dict[era_]/data_dict["generated-events"]
                     MC_hist.Add(h,factor)
                 elif data_dict['type'] == 'signal':
-                    factor = data_dict["cross-section"]*lumi_dict[self.era]* data_dict["Branching-ratio"]/data_dict["generated-events"]
+                    factor = data_dict["cross-section"]*lumi_dict[era_]* data_dict["Branching-ratio"]/data_dict["generated-events"]
                     signal_hist.Add(h,factor)
                         
-        
-            data_hist.SetLineWidth(2)
-            #data_hist.SetFillColorAlpha(colors[i], 0.35)
-            data_hist.SetLineColor(colors[i])
-            data_hist.GetXaxis().SetTitle(plot_dict['x-axis'])
-            data_hist.GetYaxis().SetTitle(plot_dict['y-axis'])
-            data_hist.SetTitle(self.variable_name)
+            if self.plot_data: 
+                data_hist.SetLineWidth(2)
+                #data_hist.SetFillColorAlpha(colors[i], 0.35)
+                data_hist.SetLineColor(colors[i])
+                data_hist.GetXaxis().SetTitle(plot_dict['x-axis'])
+                data_hist.GetYaxis().SetTitle(plot_dict['y-axis'])
+                data_hist.SetTitle(self.variable_name)
+                
+            if self.plot_signal:
+                signal_hist.SetLineWidth(2)
+                #signal_hist.SetFillColorAlpha(colors[i], 0.35)
+                signal_hist.SetLineColor(MCcolors[i])
+                signal_hist.GetXaxis().SetTitle(plot_dict['x-axis'])
+                signal_hist.GetYaxis().SetTitle(plot_dict['y-axis'])
+                #signal_hist.SetTitle(self.variable_name)
             
-            
-            signal_hist.SetLineWidth(2)
-            #signal_hist.SetFillColorAlpha(colors[i], 0.35)
-            signal_hist.SetLineColor(MCcolors[i])
-            signal_hist.GetXaxis().SetTitle(plot_dict['x-axis'])
-            signal_hist.GetYaxis().SetTitle(plot_dict['y-axis'])
-            signal_hist.SetTitle(self.variable_name)
-            
-            
-            MC_hist.SetLineWidth(2)
-            #if self.plot_MC and self.plot_data:
-            #    i *= 2
-            #    MC_hist.SetLineColor(colors[i+1])
-            #    print( "color", colors[i+1])
-            #else:
-            MC_hist.SetLineColor(MCcolors[i])
-            MC_hist.GetXaxis().SetTitle(plot_dict['x-axis'])
-            MC_hist.GetYaxis().SetTitle(plot_dict['y-axis'])
-            MC_hist.SetTitle(self.variable_name)
+            if self.plot_MC:
+                MC_hist.SetLineWidth(2)
+                #if self.plot_MC and self.plot_data:
+                #    i *= 2
+                #    MC_hist.SetLineColor(colors[i+1])
+                #    print( "color", colors[i+1])
+                #else:
+                MC_hist.SetLineColor(MCcolors[i])
+                MC_hist.GetXaxis().SetTitle(plot_dict['x-axis'])
+                MC_hist.GetYaxis().SetTitle(plot_dict['y-axis'])
+                MC_hist.SetTitle(self.variable_name)
             
             # Normalize to unity #
             if data_hist.Integral() != 0:
@@ -184,33 +189,37 @@ class StudyHighPileupRegions():
 
     def saveHistograms(self):
         num_plots = len(self.data_hist_dict.keys())*self.plot_data + len(self.MC_hist_dict.keys())*self.plot_MC + len(self.signal_hist_dict.keys())*self.plot_signal
-        #plot_ratio = num_plots==2
-        plot_ratio = False
+        
         #if num_plots>=4:
         #    legend = ROOT.TLegend(0.2,0.8,0.89,0.89)
         #    legend.SetNColumns(2)
         #    legend.SetTextSize(0.012)
-        #else:
-        legend = ROOT.TLegend(0.5,0.8,0.89,0.89)
-        legend.SetTextSize(0.02)
+        #else: (left/right, , , heigh)
+        
+        #legend = ROOT.TLegend(0.2,0.89,0.6,0.62)
+        legend = ROOT.TLegend(0.75,0.89,0.6,0.62)
+        legend.SetTextSize(0.025)
+        legend.SetBorderSize(0)
         if self.plot_MC:
             legend.SetHeader("ttbar Full-Lep + ST","C")
         elif self.plot_signal:
             #legend.SetHeader("#splitline{H->ZA->llbb }{HToZATo2L2B_MH-500_MA-300 }","C")
-            legend.SetHeader("HToZATo2L2B_MH-500_MA-300","C")
+            legend.SetHeader("#splitline{#it{ggH-resolved, %s channel}}{#it{H #rightarrow ZA #rightarrow ll bb, (MH, MA)= (500, 300) GeV}}"%channel,"C")
+            legend.SetTextAlign(12)
 
-        # Canvas and pad #
-        C = ROOT.TCanvas("c1", "c1", 600, 600)
+        C = ROOT.TCanvas("c1", "c1", 800, 800)
         pad1 = ROOT.TPad("pad1", "pad1", 0, 0.0, 1, 1.0)
+        pad1.SetTopMargin(0.1)
         pad1.SetBottomMargin(0.15)
         pad1.SetLeftMargin(0.15)
         pad1.SetRightMargin(0.1)
-        if plot_ratio:
+        if self.plot_ratio:
             pad1.SetBottomMargin(0.32)
         #pad1.SetGridx()
         #pad1.SetGridy()
         pad1.Draw()
         pad1.cd()
+        
 
         # Get Max values #
         max_data = max([h.GetMaximum() for h in self.data_hist_dict.values()])
@@ -255,10 +264,34 @@ class StudyHighPileupRegions():
                 hist_signal.Draw(opt)
                 if opt.find("same") == -1:
                     opt += " same"
-                legend.AddEntry(hist_signal,"Signal  %s "%key,'l')
+                legend.AddEntry(hist_signal,"%s"%key.lower(),'l')
+        
+        t = C.GetTopMargin()
+        r = C.GetRightMargin()
+        l = C.GetLeftMargin()
+        lumiTextOffset   = 0.2
 
+        latex = ROOT.TLatex()
+        latex.SetNDC()
+        latex.SetTextAngle(0)
+        latex.SetTextColor(ROOT.kBlack)
+
+        cms_text = latex.DrawLatex(0.15, 1-t+lumiTextOffset*t, "CMS Preliminary")
+        cms_text.SetNDC()
+        cms_text.SetTextFont(40)
+        cms_text.SetTextSize(0.03)
+        cms_text.Draw("same")
+
+        lumiText = "%s fb^{-1} (13 TeV)" %format(Constants.getLuminosity(era)/1000.,'.2f')
+        lumi = latex.DrawLatex(0.6,1-t+lumiTextOffset*t,lumiText)
+        lumi.SetNDC()
+        lumi.SetTextFont(40)
+        lumi.SetTextSize(0.03)
+        lumi.Draw("same")
+        
         legend.Draw()
-        if plot_ratio:
+        
+        if self.plot_ratio:
             keys = list(self.data_hist_dict.keys())
             if self.plot_data and not self.plot_MC:
                 hist1 = self.data_hist_dict[keys[0]]
@@ -272,6 +305,16 @@ class StudyHighPileupRegions():
             elif self.plot_signal and not self.plot_MC and not self.plot_data:
                 hist1 = self.signal_hist_dict[keys[0]]
                 hist2 = self.signal_hist_dict[keys[1]]
+
+            std1 = hist1.GetStdDev()
+            std2 = hist2.GetStdDev()
+
+            mean1 = hist1.GetMean()
+            mean2 = hist2.GetMean()
+            
+            legend.AddEntry(hist1,"#splitline{#bf{std dev #sigma :}  %s}{#bf{mean #mu    :}  %s}"%(round(std1, 2), round(mean1, 2)))
+            legend.AddEntry(hist2,"#splitline{#bf{std dev #sigma :}  %s}{#bf{mean #mu    :}  %s}"%(round(std2,2 ), round(mean2, 2)))
+            legend.Draw("Same")
             
             ratio = hist2.Clone()
             ratio.Sumw2()
@@ -317,52 +360,39 @@ class StudyHighPileupRegions():
             ratio.GetXaxis().SetLabelFont(43)
             ratio.GetXaxis().SetLabelSize(15)
 
-        C.Print(self.outname+".pdf")
-        C.Print(self.outname+".png")
+        C.Print("Run2BJetEnergyRegression/"+self.outname+".pdf")
+        C.Print("Run2BJetEnergyRegression/"+self.outname+".png")
             
-
 
 if __name__ == "__main__":
    
-#    path= '/home/ucl/cp3/kjaffel/scratch/ZAFullAnalysis/2016Results/ver.20_11_4/b-regressionOnsignals/'
-#    #path= '/home/ucl/cp3/kjaffel/scratch/ZAFullAnalysis/2016Results/ver.20_11_4/b-regressionOnbkg/'
-#    
-#    for btaggerWP in ['DeepCSVM', 'DeepFlavourM']:
-#        for var_ToPlots in ['mbb', 'mllbb', 'bjet1_pT', 'bjet2_pT', 'bjet1_mass', 'bjet2_pT']:
-#        #for var_ToPlots in ['bb_PT']:
-#            for cat in ['MuMu', 'ElEl', 'MuEl']:
-#                    
-#                variable_name = '{0} channel : {1}'.format(cat, var_ToPlots)
-#                no_bjetEnergyRegrssion = {(path,'Raw'):  '{0}_OnlyjetsPassBtagDiscrScore_MappedWithBRegCorr_resolved_{1}_{2}_METcut_bTagWeight_without_bjetEnergyRegression'.format(cat, var_ToPlots, btaggerWP)}
-#                bjetEnergyRegrssion = {(path,'regressed'):  '{0}_OnlyjetsPassBtagDiscrScore_MappedWithBRegCorr_resolved_{1}_{2}_METcut_bTagWeight_with_bjetEnergyRegression'.format(cat, var_ToPlots, btaggerWP)}
-#                
-#                instance = StudyHighPileupRegions({**no_bjetEnergyRegrssion, **bjetEnergyRegrssion},
-#                                        variable_name,"{0}_resolved_inclusive_compare_{1}_BeforeAndAfter_bregressionOnAK4BJets_{2}".format(cat, var_ToPlots, btaggerWP),
-#                                        plot_data=False, plot_MC=False, plot_signal=True)
-#
+    path = '/home/ucl/cp3/kjaffel/scratch/ZAFullAnalysis/2016Results/ver.20_11_4/b-regressionOnsignals/'
+    #path = '/home/ucl/cp3/kjaffel/scratch/ZAFullAnalysis/2016Results/ver.20_11_4/b-regressionOnbkg/'
     #path = '/home/ucl/cp3/kjaffel/scratch/ZAFullAnalysis/2017Results/ver.20_10_29/b-regression-Onsignals/'
-    path = '/home/ucl/cp3/kjaffel/scratch/ZAFullAnalysis/2017Results/ver.20_10_29/b-regression/'
-
-    #for var_ToPlots in ['mjj', 'jj_PT']:
-    #    for cat in ['MuMu', 'ElEl', 'MuEl']:
+    #path = '/home/ucl/cp3/kjaffel/scratch/ZAFullAnalysis/2017Results/ver.20_10_29/b-regression/'
+    path1 = path2 = path
+    #path1  = '../run2Ulegay_results/ul_2016__ver11/no_bjet_energy_regression__ver2/'
+    #path2  = '../run2Ulegay_results/ul_2016__ver11/with_bjet_energy_regression__ver3/'
+    #path1   = '../run2Ulegay_results/ul_2017__ver9/bjet_energy_regression_no/'
+    #path2   = '../run2Ulegay_results/ul_2017__ver8/bjet_energy_regression_yes2/'
+    era = '2016'
+    
+    for i, process in enumerate(['gg_fusion']):#, 'bb_associatedProduction']):
+        process_ = 'ggH' if i ==0 else 'bbH'
+        for var_ToPlots in ['mbb', 'mllbb', 'bjet1_pT', 'bjet2_pT', 'bjet1_mass', 'bjet2_pT']:
+            for cat in ['MuMu', 'ElEl']:
+                channel = 'ee' if cat =='ElEl' else '#mu#mu'
+                    
+                variable_name = '{} channel : {} {}'.format(cat, var_ToPlots, process_)
+                    
+                # from old files
+                no_bjetEnergyRegrssion = {(path1,'Raw'):  '{}_OnlyjetsPassBtagDiscrScore_MappedWithBRegCorr_resolved_{}_DeepFlavourM_METcut_bTagWeight_without_bjetEnergyRegression'.format(cat, var_ToPlots)}
+                bjetEnergyRegrssion = {(path2,'regressed'):  '{}_OnlyjetsPassBtagDiscrScore_MappedWithBRegCorr_resolved_{}_DeepFlavourM_METcut_bTagWeight_with_bjetEnergyRegression'.format(cat, var_ToPlots)}
                 
-    #        variable_name = '{0} channel : {1}'.format(cat, var_ToPlots)
+                # from new files
+                #no_bjetEnergyRegrssion = {(path1,'Raw'):  '{}_resolved_METCut_NobJetER_bTagWgt_{}_DeepFlavourM_{}'.format(cat, var_ToPlots, process)}
+                #bjetEnergyRegrssion = {(path2,'regressed'):  '{}_resolved_METCut_bJetER_bTagWgt_{}_DeepFlavourM_{}'.format(cat, var_ToPlots, process)}
                 
-    #        no_bjetEnergyRegrssion = {(path,'Raw'):  '{0}_resolved_{1}_without_bjetEnergyRegression'.format(cat, var_ToPlots)}
-    #        bjetEnergyRegrssion = {(path,'regressed'):  '{0}_resolved_{1}_with_bjetEnergyRegression'.format(cat, var_ToPlots)}
-    #         
-    #        instance = StudyHighPileupRegions({**no_bjetEnergyRegrssion, **bjetEnergyRegrssion},
-    #                                variable_name,"{0}_resolved_inclusive_compare_{1}_BeforeAndAfter_bregressionOnAK4Jets".format(cat, var_ToPlots),
-    #                                plot_data=False, plot_MC=False, plot_signal=True)
-    #
-    for var_ToPlots in ['mbb', 'bb_PT']:
-        for cat in ['MuEl']:
-                
-            variable_name = '{0} channel : {1}'.format(cat, var_ToPlots)
-                
-            no_bjetEnergyRegrssion = {(path,'Raw'):  '{0}_resolved_{1}_DeepCSVM_METcut_without_bjetEnergyRegression'.format(cat, var_ToPlots)}
-            bjetEnergyRegrssion = {(path,'regressed'):  '{0}_resolved_{1}_DeepCSVM_METcut_with_bjetEnergyRegression'.format(cat, var_ToPlots)}
-    ##        
-            instance = StudyHighPileupRegions({**no_bjetEnergyRegrssion, **bjetEnergyRegrssion},
-                                    variable_name,"{0}_resolved_inclusive_compare_{1}_BeforeAndAfter_bregressionOnAK4BJets".format(cat, var_ToPlots),
-                                    plot_data=True, plot_MC=True, plot_signal=False)
+                instance = BJetEnergyRegression(era, process, channel, {**no_bjetEnergyRegrssion, **bjetEnergyRegrssion},
+                                        variable_name,"{}_resolved_compare_{}_BeforeAndAfter_bjetEnergyRegression_resolved_{}".format(cat, var_ToPlots, process),
+                                        plot_data=False, plot_MC=False, plot_signal=True, plot_ratio=True)
