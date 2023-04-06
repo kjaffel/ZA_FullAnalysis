754d926bce3bf4c6b7ce0b0a43f5efca9517aaae
kjaffel <khawla.jaffel@cern.ch>
2023-04-06 14:35:50 +0200

    keep only needed categories from now on

--- ZAStatAnalysis/Combine4Slurm.py
+++ ZAStatAnalysis/Combine4Slurm.py
@@ -13,6 +13,7 @@ def SlurmCombine(cardDir, method, outDir, time, mem_per_cpu, isTest):
     config.sbatch_chdir = os.path.join(outDir, 'slurm', method)
     config.sbatch_time  = time
     config.sbatch_memPerCPU = mem_per_cpu
+    config.sbatch_additionalOptions=['--exclude=mb-ivy220']
     #config.environmentType = "cms"
     #config.inputSandboxContent = [""]
     #config.stageoutFiles = ['*.root']
@@ -28,19 +29,19 @@ def SlurmCombine(cardDir, method, outDir, time, mem_per_cpu, isTest):
         if isTest and i!=0:
             continue
         m = inF.split('/')[-2]
-        #if not script in [ "HToZATo2L2B_gg_fusion_nb2PLusnb3_resolved_boosted_split_OSSF_MuEl_dnn_MH_500.0_MA_300.0_run_goodness_of_fit.sh",
-        #                   "HToZATo2L2B_bb_associatedProduction_nb2PLusnb3_resolved_boosted_split_OSSF_MuEl_dnn_MH_500.0_MA_300.0_run_goodness_of_fit.sh",
-        #                   "HToZATo2L2B_gg_fusion_nb2PLusnb3_resolved_boosted_split_OSSF_MuEl_dnn_MH_300.0_MA_200.0_run_goodness_of_fit.sh",
-        #                   "HToZATo2L2B_bb_associatedProduction_nb2PLusnb3_resolved_boosted_split_OSSF_MuEl_dnn_MH_300.0_MA_200.0_run_goodness_of_fit.sh",
-        #                   "HToZATo2L2B_bb_associatedProduction_nb2PLusnb3_resolved_boosted_OSSF_MuEl_dnn_MH_717.96_MA_577.65_run_goodness_of_fit.sh",
-        #                   "HToZATo2L2B_gg_fusion_nb2PLusnb3_resolved_boosted_split_OSSF_MuEl_dnn_MH_650.0_MA_50.0_run_goodness_of_fit.sh",
-        #                   "HToZATo2L2B_bb_associatedProduction_nb2PLusnb3_resolved_boosted_split_OSSF_MuEl_dnn_MH_650.0_MA_50.0_run_goodness_of_fit.sh",
-        #                   "HToZATo2L2B_bb_associatedProduction_nb2PLusnb3_resolved_boosted_split_OSSF_dnn_MH_650.0_MA_50.0_run_goodness_of_fit.sh",
-        #                   "HToZATo2L2B_gg_fusion_nb2PLusnb3_resolved_boosted_split_OSSF_MuEl_dnn_MH_800.0_MA_200.0_run_goodness_of_fit.sh",
-        #                   "HToZATo2L2B_bb_associatedProduction_nb2PLusnb3_resolved_boosted_split_OSSF_MuEl_dnn_MH_800.0_MA_200.0_run_goodness_of_fit.sh"]:
-        #    continue
-        #if not m in ['MA-800.0_MH-140.0', 'MH-650.0_MA-50.0', 'MH-300.0_MA-200.0', 'MH-379.0_MA-54.59', 'MH-500.0_MA-300.0', 'MH-510.0_MA-130.0', 'MA-510.0_MH-130.0', 'MH-800.0_MA-200.']:
-        #    continue
+        
+        if not ( script.startswith("HToZATo2L2B_gg_fusion_nb2_resolved_boosted_OSSF_MuEl_dnn_") or 
+                 script.startswith("HToZATo2L2B_gg_fusion_nb2_resolved_MuMu_ElEl_MuEl_dnn_") or 
+                 script.startswith("HToZATo2L2B_gg_fusion_nb2_boosted_OSSF_MuEl_dnn_") or 
+                 script.startswith("HToZATo2L2B_bb_associatedProduction_nb2PLusnb3_resolved_boosted_OSSF_MuEl_dnn_") or
+                 script.startswith("HToZATo2L2B_bb_associatedProduction_nb2PLusnb3_resolved_OSSF_MuEl_dnn_") or
+                 script.startswith("HToZATo2L2B_bb_associatedProduction_nb2PLusnb3_boosted_OSSF_MuEl_dnn_") 
+                 ):
+            continue
+        
+        if not m in ['MH-300.0_MA-200.0']:#, 'MA-800.0_MH-140.0', 'MH-650.0_MA-50.0', 'MH-300.0_MA-200.0', 'MH-379.0_MA-54.59', 'MH-500.0_MA-300.0', 'MH-510.0_MA-130.0', 'MA-510.0_MH-130.0', 'MH-800.0_MA-200.']:
+            continue
+
         config.inputParams.append([cmssw, dir, script])
     config.payload = \
         """
