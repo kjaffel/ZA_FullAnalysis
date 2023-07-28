d93114ea0d555a20e4ad0bd3e00a74ed944f3e08
kjaffel <khawla.jaffel@cern.ch>
2023-07-28 12:00:18 +0200

    small fix

--- bamboo_/run_bamboO.sh
+++ bamboo_/run_bamboO.sh
@@ -2,52 +2,22 @@
 
 run='--distributed=driver'
 #run='--maxFiles=1'
+#run='--onlyprepare'              # useful to update cacheJEC!
 #run='--distributed=finalize'
 
-#yml='config/fullanalysisRunIISummer20UL_18_17_16_nanov9_chunk1_for_unblindstage1.yml'
-#yml='config/fullanalysisRunIISummer20UL_18_17_16_nanov9_custom_for_unblindstage1.yml'
-#yml='config/mc_fullanalysisRunIISummer20UL_18_17_16_nanov9_for_btagEffMaps.yml'
-#yml='config/fullanalysisRunIISummer20UL_18_17_16_nanov9_noSignal.yml'
-#yml='config/fullanalysisRunIISummer20UL_18_17_16_nanov9_FewSignals.yml'
-#yml='config/fullanalysisRunIISummer20UL_18_17_16_nanov9_for_skim.yml'
 #yml='config/llbbtest_UL16_nanov9.yml'
-#yml='config/fullanalysisRunIISummer20UL_18_17_16_nanov9_for_dy_ttbar_split.yml'
 #yml='config/data_fullanalysisRunIISummer20UL_18_17_16_nanov9.yml'
 
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext4/missing/'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext5/btagEffmaps'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext5/sanitycheck__4'
-#output='llbbtests/test_new_changes__3'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext6/sanitycheck__2'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext6/sanitycheck__3'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext6/sanitycheck__4'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext6/sanitycheck__5'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext6/sanitycheck__6'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext6/sanitycheck__7'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext6/sanitycheck__8'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext6/sanitycheck__9'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext6/sanitycheck__10/btagEffmaps'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext6/sanitycheck__10/forcombine'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext6/sanitycheck__11'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext7/forcombine/'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext7/skim/'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext8/fix_pre_postVFP/'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext9/fix_pre_postVFP/'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext11'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext12'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext13'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext14'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext15'
-#output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext17/data/'
-output='run2Ulegay_results/unblind_stage1_full_per_chunk_fullrun2/ext19'
+#output='run2Ulegay_results/unblind_stage2_full_per_chunk_fullrun2/ver0'
+output='run2Ulegay_results/unblind_stage2_full_per_chunk_fullrun2/ver2'
 
-era='' #choices : 2016-preVFP, 2016-postVFP, 2017, 2018, '' will do full run2 (if all eras are provided in the yml)
+#era='2016-preVFP' #choices : 2016-preVFP, 2016-postVFP, 2017, 2018, '' will do full run2 (if all eras are provided in the yml)
 eras=('2016-preVFP' '2016-postVFP' '2017' '2018' )
 do_what='analysis' #choices : 'btag' or 'analysis'
 
 doSkim=false
 doSysts=true
-doEvaluate=true
+doEvaluate=true   # evaluate DNN 
 doChunk=true
 
 if [ "$do_what" == 'btag' ]; then
@@ -62,24 +32,21 @@ if [ "$era" != '' ]; then
     plus_args=' --era='$era
 fi
 
-
 if $doSkim; then
     plus_args+=' --skim'
 fi
-
 
 if $doEvaluate; then
     plus_args+=' -dnn'
 fi
 
-
 if $doSysts; then
     plus_args+=' -s'
 fi
 
-
+## https://stackoverflow.com/questions/1908610/how-to-get-process-id-of-background-process
 if $doChunk; then
-    for chunk in $(seq 10 19); do
+    for chunk in $(seq 11 19); do  # me $(seq 0 9)  && sjain will do $(seq 10 19)
         yml='config/fullanalysisRunIISummer20UL_18_17_16_chunk'${chunk}'_nanov9.yml'
         for era in ${eras[*]}; do
             
@@ -89,10 +56,31 @@ if $doChunk; then
             #
             _output=${output}/$era/'chunk_'${chunk}
             plus_args_loop+=' --era='$era' --chunk='$chunk
-        
-            echo "Will do: bambooRun $run -m $module $yml -o ${_output} $plus_args_loop"
-            bambooRun $run -m $module $yml -o ${_output} $plus_args_loop &
+             
+            # just do it once, otherwise will take lots of time!
+            if [ "$run" = "--distributed=finalize" ]; then
+                pushd ${_output}/infiles
+                sed -i 's|root://xrootd-cms\..*\.it//|/storage/data/cms|g' *.txt
+                popd
+            fi
+            
+            echo " JOBPID=$$ Will do: bambooRun $run -m $module $yml -o ${_output} $plus_args_loop " #&> logs/logs_${era}_chunk_${chunk}.txt"
+            #bambooRun $run -m $module $yml -o ${_output} $plus_args_loop & #> logs/logs_${era}_chunk_${chunk}.txt &
         done
         echo '==========================='
     done
+
+else
+    echo "Will do: bambooRun $run -m $module $yml -o ${output} $plus_args"
+    bambooRun $run -m $module $yml -o ${output} $plus_args
 fi
+
+# slurm will complain for long jobs during re-submission of the failed ones,
+# the followinf will break the list of long jobs instead, So proceed by running logs/redo_best.sh
+if [ "$run" = "--distributed=finalize" ]; then
+    find logs/*.txt -type f | xargs grep  "sbatch --array" >& logs/redo.sh
+    sed -i "s/.*Resubmit with '//g" logs/redo.sh
+    sed -i "s/' (and possibly additional options)*//" logs/redo.sh
+    python breakLongJobs.py
+fi
+
